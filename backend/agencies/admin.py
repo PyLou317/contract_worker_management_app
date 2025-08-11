ae04a944c9946fd46462a210ddcc1f24634a345f
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-10 22:15:37 -0700

    Add agency model and admin

--- backend/agencies/admin.py
+++ backend/agencies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from agencies.models import StaffingAgency
 
-# Register your models here.
+admin.site.register(StaffingAgency)
+
--- backend/agencies/models.py
+++ backend/agencies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class StaffingAgency(models.Model):
+    name = models.CharField(max_length=255)
+    contact_email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    province = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
