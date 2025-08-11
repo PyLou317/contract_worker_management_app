3d2e09c318eff4d40c98cec746b4e601dd1bdca7
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-10 22:16:01 -0700

    Change employer name to current_contract

--- backend/workers/models.py
+++ backend/workers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from organizations.models import WarehouseBusiness
+from agencies.models import StaffingAgency
 
 
 # Create your models here.
@@ -8,8 +9,8 @@ class ContractWorker(models.Model):
     last_name = models.CharField(max_length=125)
     email = models.EmailField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=20)
-    employer = models.ForeignKey(WarehouseBusiness, on_delete=models.CASCADE)
-
+    current_contract = models.ForeignKey(WarehouseBusiness, on_delete=models.CASCADE)
+    agency = models.ForeignKey(StaffingAgency, on_delete=models.CASCADE)
     
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
