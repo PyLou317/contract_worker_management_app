89a5a0f4e8680500b2c853e6e36df661fdeb6c61
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-17 15:59:30 -0700

    Adjust Serializer to get average ratting and comments for each worker

--- backend/workers/serializers.py
+++ backend/workers/serializers.py
@@ -2,7 +2,24 @@ from rest_framework import serializers
 from .models import *
 
 class ContractWorkerSerializer(serializers.ModelSerializer):
+    average_rating = serializers.SerializerMethodField()
+    agency = serializers.StringRelatedField()
+    current_contract = serializers.StringRelatedField()
+    comment = serializers.SerializerMethodField()
+    
     class Meta:
         model = ContractWorker
-        fields = '__all__'
-        depth = 1
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone_number', 'current_contract', 'agency', 'average_rating', 'comment')
+        
+    def get_average_rating(self, obj):
+        ratings = obj.ratings.all()
+        if ratings:
+            total_score = sum(rating.get_average_score() for rating in ratings)
+            return total_score / ratings.count()
+        return 0
+    
+    def get_comment(self, obj):
+        ratings = obj.ratings.all()
+        if ratings:
+            return ratings[0].comment
+        return None
