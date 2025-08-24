f4deef7a627da56d35eaf5ffc930b585c35f7b0d
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-23 20:07:19 -0700

    Allow search filters on workers api

--- backend/workers/views.py
+++ backend/workers/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import filters
 from .models import *
 from .serializers import *
 
@@ -8,3 +9,6 @@ class ContractWorkerListViewAPI(generics.ListCreateAPIView):
     queryset = ContractWorker.objects.all()
     serializer_class = ContractWorkerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['first_name', 'last_name', 'agency__name']
