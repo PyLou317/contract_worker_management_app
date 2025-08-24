2a1375c983d5138e2c4afd29ea375e6c06d55e92
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-23 20:14:14 -0700

    Add logout button and function

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,12 +1,21 @@
 export default function TopNavBar() {
+    const handleLogout = () => {
+      
+  };
+
   return (
     <nav className="bg-gray-900 p-4 text-white">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold cursor-pointer">HIVE</h1>
         <div className="flex space-x-4">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer">Login</button>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer"
+            onClick={handleLogout}
+          >
+            Login
+          </button>
         </div>
       </div>
     </nav>
   );
-} 
+}
