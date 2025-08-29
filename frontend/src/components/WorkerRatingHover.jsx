b0bf7e76e0c20f12516266fe0bb6c26606962793
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-29 06:54:23 -0700

    Remove

--- frontend/src/components/WorkerRatingHover.jsx
+++
@@ -1,7 +0,0 @@
-export default function RatingHover({ rating }) {
-    return (
-        <dialog className="rounded-md shadow-sm bg-white p-4">
-            <span>{rating}</span>
-        </dialog>
-    )
-}
