12e2afa50cc1088d9d3268aa099b949c76457ab8
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-29 06:53:11 -0700

    Order by first name and allow for ascending and descending ordering by rating

--- backend/workers/views.py
+++ backend/workers/views.py
@@ -1,14 +1,23 @@
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import filters
+from django.db.models import Avg
 from .models import *
 from .serializers import *
 
-# Create your views here.
 class ContractWorkerListViewAPI(generics.ListCreateAPIView):
     queryset = ContractWorker.objects.all()
     serializer_class = ContractWorkerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['first_name', 'last_name', 'agency__name']
+    ordering_fields = ['first_name', 'last_name', 'avg_rating']
+    ordering = ['first_name']
+    
+    def get_queryset(self):
+        # Annotate the queryset with the average rating.
+        queryset = super().get_queryset()
+        return queryset.annotate(
+            avg_rating=Avg('ratings__performance_score')
+        )
