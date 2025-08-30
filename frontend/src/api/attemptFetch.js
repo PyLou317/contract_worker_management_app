2859e8b0d848d842d0463a19b660d4a9928e24d0
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-29 17:01:15 -0700

    Move to own file

---
+++ frontend/src/api/attemptFetch.js
@@ -0,0 +1,24 @@
+const attemptFetch = async (token, formData, endpoint) => {
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(formData),
+  });
+
+  if (response.status === 401) {
+    throw new Error('Unauthorized');
+  }
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    console.error('Server error details:', errorData);
+    throw new Error('Failed to add worker');
+  }
+
+  return await response.json();
+};
+
+export default attemptFetch;
