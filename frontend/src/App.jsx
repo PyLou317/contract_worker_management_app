8d6c9ce49e95e23b9e7185cd54e262c97a69f299
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-30 23:14:42 -0700

    Add warning modal for logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SideNavBar from './components/SideNavBar';
 import Dashboard from './pages/DashboardPage';
 import Workers from './pages/WorkersPage';
 import Login from './components/LoginPage';
+import WarningModal from './components/WarningModal';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -14,6 +15,7 @@ const queryClient = new QueryClient();
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isActive, setIsActive] = useState('Dashboard');
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     // Check for the auth token when the app loads
@@ -32,18 +34,24 @@ function App() {
   } else {
     return (
       <QueryClientProvider client={queryClient}>
-        <div className="flex flex-col h-screen">
-          <TopNavBar />
-          <div className="flex flex-1">
-            <SideNavBar activeLink={isActive} onLinkClick={handleSideLinkClick} />
-            <div className="m-4 flex-1 flex-col justify-start items-start h-screen w-screen">
-              <div>
-                {isActive === 'Dashboard' && <Dashboard />}
-                {isActive === 'Employees' && <Workers />}
-                {/* {isActive === 'Scheduling' &&  <Scheduling />}
+        <div className="flex w-full min-h-screen">
+          <SideNavBar
+            activeLink={isActive}
+            onLinkClick={handleSideLinkClick}
+            onLogoutClick={() => {setShowModal(true); console.log('Logout clicked')}}
+          />
+          <WarningModal show={showModal} onClose={() => setShowModal(false)}>
+            <p className="text-lg font-semibold">Are you sure you want to log out?</p>
+            <p className="text-sm text-gray-400 mt-2">You will need to sign in again to access your account.</p>
+          </WarningModal>
+          <div className="flex flex-col flex-1 h-screen overflow-y-auto">
+            <TopNavBar />
+            <main className="m-4 flex-1">
+              {isActive === 'Dashboard' && <Dashboard />}
+              {isActive === 'Employees' && <Workers />}
+              {/* {isActive === 'Scheduling' &&  <Scheduling />}
               {isActive === 'Settings' && <Settings />} */}
-              </div>
-            </div>
+            </main>
           </div>
         </div>
         <ReactQueryDevtools initialIsOpen={false} />
