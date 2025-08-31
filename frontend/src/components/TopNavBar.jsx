7de6ae37228ffd8bf1e850c894f0fddf830d5b04
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-30 23:16:51 -0700

    Remove logout button

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -8,18 +8,8 @@ export default function TopNavBar() {
   };
 
   return (
-    <nav className="bg-gray-900 p-4 text-white">
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold cursor-pointer">HIVE</h1>
-        <div className="flex space-x-4">
-          <button
-            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        </div>
-      </div>
+    <nav className="p-6 border-b-2 border-gray-200 w-full bg-white flex justify-between items-center space-x-4">
+      <h1 className="text-2xl font-semibold">Welcome, Lucas!</h1>
     </nav>
   );
 }
