dd9bd0cedb820f089594534786583803413a3a92
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-30 23:15:19 -0700

    Add Brand component

---
+++ frontend/src/components/Brand.jsx
@@ -0,0 +1,12 @@
+export default function BrandLogoAndName({ className }) {
+  return (
+    <>
+      <h1 className={className}>
+        <span className="text-yellow-100">
+          <i className="fa-solid fa-hexagon"></i>
+        </span>{' '}
+        the HIVE
+      </h1>
+    </>
+  );
+}
