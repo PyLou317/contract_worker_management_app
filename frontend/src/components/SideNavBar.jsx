9d0f2970fb57c9ea866f539dc6e104a4e6417889
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-08-30 23:16:38 -0700

    Add icon to sidebar links

--- frontend/src/components/SideNavBar.jsx
+++ frontend/src/components/SideNavBar.jsx
@@ -1,30 +1,93 @@
 import { useState } from 'react';
+import Brand from './Brand';
 
-export default function SideNavBar({ activeLink, onLinkClick }) {
-    const menuItems = [
-        { name: 'Dashboard' },
-        { name: 'Employees' },
-        { name: 'Scheduling' },
-        { name: 'Settings' }
-    ];
+export default function SideNavBar({ activeLink, onLinkClick, onLogoutClick }) {
+  const menuItems = [
+    {
+      name: 'Dashboard',
+      icon: <i className="fa-solid fa-chart-line"></i>,
+    },
+    {
+      name: 'Employees',
+      icon: <i className="fa-solid fa-users"></i>,
+    },
+    {
+      name: 'Scheduling',
+      icon: <i className="fa-solid fa-calendar"></i>,
+    },
+    {
+      name: 'Settings',
+      icon: <i className="fa-solid fa-gear"></i>,
+    },
+    {
+      name: 'Logout',
+      icon: <i className="fa-solid fa-right-from-bracket"></i>,
+    },
+  ];
+
+  const mainItems = menuItems.slice(0, 3);
+  const settingsItem = menuItems.slice(3, 5);
+  console.log(settingsItem);
 
   return (
-    <nav className="bg-gray-800 w-64 p-4 text-white">
-      <div className="flex flex-col items-start">
-        <ul className="w-full">
-          {menuItems.map((item, index) => (
+    <>
+      <nav className="bg-gray-800 w-64 text-white p-4 h-screen overflow-y-auto flex flex-col">
+        <div className="border-b-2 border-gray-700 py-4">
+          <Brand className="text-2xl" />
+        </div>
+        <ul className="w-full flex-1">
+          {mainItems.map((item, index) => (
             <li
               key={index}
-              className={`my-2 p-4 hover:bg-gray-700 ${
+              className={`flex my-2 p-4 hover:bg-gray-700 gap-4 ${
                 activeLink === item.name ? 'bg-gray-700' : 'hover:bg-gray-700'
               } cursor-pointer rounded-lg`}
               onClick={() => onLinkClick(item.name)}
             >
-              {item.name}
+              <span
+                className={`${
+                  activeLink === item.name
+                    ? 'bg-gradient-to-b from-blue-300 to-blue-600 bg-clip-text text-transparent'
+                    : 'text-white group-hover:bg-gradient-to-b group-hover:from-blue-300 group-hover:to-blue-600 group-hover:bg-clip-text group-hover:text-transparent'
+                }`}
+              >
+                {item.icon}
+              </span>
+              <span>{item.name}</span>
             </li>
           ))}
         </ul>
-      </div>
-    </nav>
+
+        <ul className="w-full mt-auto">
+          {settingsItem.map((item, index) => (
+            <li
+              key={index}
+              className={`flex my-2 p-4 gap-4 ${
+                activeLink === item.name ? 'bg-gray-700' : 'hover:bg-gray-700'
+              } cursor-pointer rounded-lg`}
+              onClick={() => {
+                onLinkClick(item.name);
+                {
+                  if (item.name === 'Logout') {
+                    {onLogoutClick(); console.log('Logout clicked')}
+                  }
+                }
+              }}
+            >
+              <span
+                className={`${
+                  activeLink === item.name
+                    ? 'bg-gradient-to-b from-blue-300 to-blue-600 bg-clip-text text-transparent'
+                    : 'text-white group-hover:bg-gradient-to-b group-hover:from-blue-300 group-hover:to-blue-600 group-hover:bg-clip-text group-hover:text-transparent'
+                }`}
+              >
+                {item.icon}
+              </span>
+              <span>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 }
