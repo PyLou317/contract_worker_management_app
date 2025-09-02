ff77c4c36a8e2ad454a5b18be7c7fdcc09dea881
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-01 20:18:39 -0700

    Add worker position choices

--- backend/workers/models.py
+++ backend/workers/models.py
@@ -3,6 +3,12 @@ from organizations.models import WarehouseBusiness
 from agencies.models import StaffingAgency
 
 
+WORKER_POSITION = [
+    ('forklift_operator', 'Forklift Operator'),
+    ('warehouse_associate', 'Warehouse Associate'),
+    ('material_handler', 'Material Handler')
+]
+
 # Create your models here.
 class ContractWorker(models.Model):
     first_name = models.CharField(max_length=125)
@@ -11,6 +17,7 @@ class ContractWorker(models.Model):
     phone_number = models.CharField(max_length=20)
     current_contract = models.ForeignKey(WarehouseBusiness, on_delete=models.CASCADE)
     agency = models.ForeignKey(StaffingAgency, on_delete=models.CASCADE)
+    position = models.CharField(max_length=125, choices=WORKER_POSITION, default='Warehouse Associate')
     
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
