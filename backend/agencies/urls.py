e90a8b2f90cbe72d0223ff0ee1996ddba7bab6af
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-01 20:18:15 -0700

    Add agency api url and view

--- backend/agencies/urls.py
+++ backend/agencies/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
-from . import views
+from .views import *
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-
+    path('agencies/', AgencyListViewAPI.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/agencies/views.py
+++ backend/agencies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from .models import StaffingAgency
+from .serializers import StaffingAgencySerializer
 
 # Create your views here.
+
+class AgencyListViewAPI(generics.ListCreateAPIView):
+    queryset = StaffingAgency.objects.all()
+    serializer_class = StaffingAgencySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
