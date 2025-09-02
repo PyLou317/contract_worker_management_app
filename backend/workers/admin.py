ba305a4519af48c1cd55c27ae8edce6d77411cec
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-01 20:18:27 -0700

    Customize admin

--- backend/workers/admin.py
+++ backend/workers/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from workers.models import ContractWorker
 
 # Register your models here.
-admin.site.register(ContractWorker)
+@admin.register(ContractWorker)
+class ContractWorkerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'current_contract', 'agency', 'position')
+    list_display_links = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'email', 'phone_number', 'current_contract__name', 'agency__name', 'position')
+    list_filter = ('agency', 'position')
+    ordering = ('last_name', 'first_name')
+    readonly_fields = ('id',)
+
