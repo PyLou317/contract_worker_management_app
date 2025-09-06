9d84d7224efe046058538b3bff4019423d9415e3
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-05 22:22:46 -0600

    Add feature: color selector

---
+++ frontend/src/components/SkillsPage/ColorSelector.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import skillColorClasses from '../WorkerListPage/SkillClasses';
+
+const colors = Object.keys(skillColorClasses);
+
+export default function ColorSelector({ onColorChange }) {
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+
+  const handleColorChange = (e) => {
+    setSelectedColor(e.target.value);
+    onColorChange(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-row space-y-4 gap-4">
+      <div>
+        <label className="block text-gray-700 text-xs font-bold mb-2" htmlFor="color">
+          Base Color
+        </label>
+        <select
+          className="border rounded-lg p-2 w-full h-[40px] text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          id="color"
+          name="color"
+          value={selectedColor}
+          onChange={handleColorChange}
+          required
+        >
+          {colors.map((color) => (
+            <option key={color} value={color}>
+              {color}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex justify-center items-center mt-2">
+        <div
+          className={`h-10 w-10 rounded-lg border border-gray-300 shadow-lg transition-all duration-300 ${skillColorClasses[selectedColor][1]}`}
+        ></div>
+      </div>
+    </div>
+  );
+}
