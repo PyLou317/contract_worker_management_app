c5a15fea88f24095f0d2ab6d67bc00e7b17f0e77
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-10 21:34:39 -0700

    .

--- frontend/src/api/addSkill.js
+++ frontend/src/api/addSkill.js
@@ -1,5 +1,5 @@
 import refreshToken from './refreshToken';
-import attemptFetchPost from './attemptFetch';
+import attemptFetchPost from './addWorkerAttemptFetch';
 
 const endpoint = `${import.meta.env.VITE_API_URL}/skills/`;
 
