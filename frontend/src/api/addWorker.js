39bde50602d0fe631b0dcd924e97719402506604
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-10 21:32:18 -0700

    Move attempt fetch to same file

--- frontend/src/api/addWorker.js
+++ frontend/src/api/addWorker.js
@@ -1,17 +1,39 @@
 import refreshToken from '../api/refreshToken';
-import attemptFetchPost from '../api/attemptFetch';
 
 const endpoint = `${import.meta.env.VITE_API_URL}/workers/`;
 
+const attemptFetch = async (token, formData, endpoint) => {
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(formData),
+  });
+
+  if (response.status === 401) {
+    throw new Error('Unauthorized');
+  }
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    console.error('Server error details:', errorData);
+    throw new Error('Failed to send data to the server.');
+  }
+
+  return await response.json();
+};
+
 const addWorker = async (formData) => {
-  let authToken = localStorage.getItem('authToken');
+  const authToken = localStorage.getItem('authToken');
 
   if (!authToken) {
     throw new Error('Authentication token not found. Please log in.');
   }
 
   try {
-    return await attemptFetchPost(authToken, formData, endpoint);
+    return await attemptFetch(authToken, formData, endpoint);
   } catch (error) {
     console.error('Error adding worker:', error);
 
@@ -26,7 +48,7 @@ const addWorker = async (formData) => {
       console.log('Token expired. Attempting to refresh...');
       try {
         const newAuthToken = await refreshToken();
-        return await attemptFetchPost(newAuthToken, formData, endpoint);
+        return await attemptFetch(newAuthToken, formData, endpoint);
       } catch (refreshError) {
         throw new Error('Session expired. Please log in again.');
       }
