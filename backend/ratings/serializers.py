e972d0ae75a52b50eca2799ac5e9f8e0b1bb39a1
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-13 07:26:48 -0700

    Make ID writeable to create new rating when worker doesn't have one assigned

--- backend/ratings/serializers.py
+++ backend/ratings/serializers.py
@@ -7,6 +7,7 @@ class NestedManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'first_name', 
             'last_name', 
             'email', 
@@ -18,10 +19,12 @@ class NestedManagerSerializer(serializers.ModelSerializer):
 class RatingSerializer(serializers.ModelSerializer):
     manager = NestedManagerSerializer()
     average_rating = serializers.SerializerMethodField()
+    id = serializers.IntegerField(read_only=False, required=False, allow_null=True)
     
     class Meta:
         model = Rating
         fields = [
+            'id',
             'average_rating',
             'attendance_score',
             'performance_score',
