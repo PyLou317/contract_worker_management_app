48bb786aa98a59ee8c99396ad158c2a8f271f07c
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-15 07:15:49 -0700

    Increase header font size

--- frontend/src/components/WorkerListPage/WorkersPage.jsx
+++ frontend/src/components/WorkerListPage/WorkersPage.jsx
@@ -23,7 +23,7 @@ export default function Workers() {
 
   return (
     <div className="container mx-auto p-8 bg-white shadow-xl rounded-2xl">
-      <h2 className="text-2xl font-bold mb-4 text-gray-800">Employee Roster</h2>
+      <h2 className="text-2xl font-bold mb-8 text-gray-800">Employee Roster</h2>
       <Header handleSearch={handleSearch} handleOpenAddWorkerModal={handleOpenAddWorkerModal} searchTerm={searchTerm} />
       <WorkerListTable searchTerm={searchTerm} page={page} setPage={setPage} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </div>
