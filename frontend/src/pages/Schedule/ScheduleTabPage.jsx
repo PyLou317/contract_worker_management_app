983ac88c72b100ea54ef90ecfc523935838d2df9
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-17 21:51:34 -0700

    Add Schedule tab component

---
+++ frontend/src/pages/Schedule/ScheduleTabPage.jsx
@@ -0,0 +1,11 @@
+import CreateScheduleForm from './CreateScheduleForm';
+import ScheduleList from './ScheduleList';
+
+export default function ScheduleTabPage() {
+  return (
+    <>
+      <CreateScheduleForm />
+      <ScheduleList />
+    </>
+  );
+}
