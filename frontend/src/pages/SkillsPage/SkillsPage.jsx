e55d9fddbf618a3df582486bb188071e5238a869
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-17 21:53:47 -0700

    Update functions

--- frontend/src/pages/SkillsPage/SkillsPage.jsx
+++ frontend/src/pages/SkillsPage/SkillsPage.jsx
@@ -1,31 +1,46 @@
+import { useState } from 'react';
 import { SkillsContext } from './skills-page-ctx';
 
-import AddSkillForm from './AddSkill';
+import AddSkillForm from './AddSkillForm';
 import SkillsList from './SkillsList';
+import EditSkillModal from './EditSkillModal';
 
 export default function SkillsPage() {
-  const handleOpenEditSkillModal = () => {
-    console.log('Opening edit skill modal for skill ID:', skillId);
-    onClose();
+  const [showEditSkillModal, setShowEditSkillModal] = useState(false);
+
+  const onClose = () => {
+    setShowEditSkillModal(false);
   };
 
-  const handleOpenDeleteSkillModal = () => {
+  const handleOpenEditSkillModal = (skillId) => {
+    console.log('Opening edit skill modal for skill ID:', skillId);
+    setShowEditSkillModal(true);
+  };
+
+  const handleOpenDeleteSkillModal = (skillId) => {
     console.log('Opening delete skill modal for skill ID:', skillId);
-    onClose();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   const ctxValue = {
+    showEditSkillModal: showEditSkillModal,
+    onClose: onClose,
     openEditSkillModal: handleOpenEditSkillModal,
     openDeleteSkillModal: handleOpenDeleteSkillModal,
+    handleSubmit: handleSubmit,
   };
 
   return (
     <SkillsContext.Provider value={ctxValue}>
-      <div className="container mx-auto p-8 bg-white">
+      <div className="container mx-auto p-4 bg-white">
         <AddSkillForm />
         <div className="mt-8 w-full border-t border-gray-200"></div>
         <SkillsList />
       </div>
+      <EditSkillModal />
     </SkillsContext.Provider>
   );
 }
