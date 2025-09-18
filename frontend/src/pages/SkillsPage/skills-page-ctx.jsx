a55b9211e05367ad9fcadb017f9ebd05e476a941
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-17 21:19:27 -0700

    Add functions

--- frontend/src/pages/SkillsPage/skills-page-ctx.jsx
+++ frontend/src/pages/SkillsPage/skills-page-ctx.jsx
@@ -3,4 +3,7 @@ import { createContext } from "react";
 export const SkillsContext = createContext({
     openEditSkillModal: () => { },
     openDeleteSkillModal: () => { },
+    showEditSkillModal: false,
+    handleCloseActionMenu: () => { },
+    handleSubmit: () => { },
 });
