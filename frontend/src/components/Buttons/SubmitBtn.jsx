8201cbcb65449df58ecee57728cf0981259cac9f
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-19 12:41:41 -0700

    Passed props to component

--- frontend/src/components/Buttons/SubmitBtn.jsx
+++ frontend/src/components/Buttons/SubmitBtn.jsx
@@ -1,10 +1,11 @@
-export default function SubmitBtn({ label, disabled, handleSubmit }) {
+export default function SubmitBtn({ label, disabled, handleSubmit, ...props }) {
   return (
     <button
       type="submit"
       className="px-4 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition-colors"
       disabled={disabled}
       onClick={handleSubmit}
+      {...props}
     >
       {label}
     </button>
