d18c8eef0c416aaea81a32db04ac1777dc1d5fb8
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-20 22:32:26 -0700

    Moved files

---
+++ frontend/src/hooks/addWorker.js
@@ -0,0 +1,61 @@
+import refreshToken from '@/hooks/refreshToken';
+
+const endpoint = `${import.meta.env.VITE_API_URL}/workers/`;
+
+const attemptFetch = async (token, formData, endpoint) => {
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(formData),
+  });
+
+  if (response.status === 401) {
+    throw new Error('Unauthorized');
+  }
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    console.error('Server error details:', errorData);
+    throw new Error('Failed to send data to the server.');
+  }
+
+  return await response.json();
+};
+
+const addWorker = async (formData) => {
+  const authToken = localStorage.getItem('authToken');
+
+  if (!authToken) {
+    throw new Error('Authentication token not found. Please log in.');
+  }
+
+  try {
+    return await attemptFetch(authToken, formData, endpoint);
+  } catch (error) {
+    console.error('Error adding worker:', error);
+
+    // Check if the error has a response and it's not a 401
+    if (error.response && error.response.status !== 401) {
+      // If it's a validation error, parse and re-throw the specific error data
+      const errorData = await error.response.json();
+      throw new Error(errorData.email[0] || 'Validation error');
+    }
+
+    if (error.message === 'Unauthorized') {
+      console.log('Token expired. Attempting to refresh...');
+      try {
+        const newAuthToken = await refreshToken();
+        return await attemptFetch(newAuthToken, formData, endpoint);
+      } catch (refreshError) {
+        throw new Error('Session expired. Please log in again.');
+      }
+    } else {
+      throw error;
+    }
+  }
+};
+
+export default addWorker;
---
+++ frontend/src/hooks/refreshToken.js
@@ -0,0 +1,28 @@
+const refreshToken = async () => {
+  const refreshToken = localStorage.getItem('refreshToken');
+  console.log('Refresh Token: ' + refreshToken);
+
+  if (!refreshToken) {
+    throw new Error('Refresh token not found. Please log in again.');
+  }
+
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/token/refresh/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ refresh: refreshToken }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to refresh token');
+  }
+
+  const data = await response.json();
+  localStorage.setItem('authToken', data.access);
+  return data.access;
+};
+
+export default refreshToken;
+
+
