d7cd72a196d3dffa568b1dcd7c85a0548837935f
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-28 06:40:31 -0700

    Add organization filtering for views

--- backend/scheduling/views.py
+++ backend/scheduling/views.py
@@ -27,14 +27,50 @@ class ShiftListViewAPI(generics.ListCreateAPIView):
     serializer_class = ShiftSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Schedule.objects.none()
+
+        user_organization = user.organization
+        if not user_organization:
+            return Schedule.objects.none()
+
+        queryset = super().get_queryset().filter(schedule__organization=user_organization)
+        return queryset
+    
 
 class AreaListViewAPI(generics.ListCreateAPIView):
     queryset = Area.objects.all()
     serializer_class = AreaSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Area.objects.none()
+
+        user_organization = user.organization
+        if not user_organization:
+            return Area.objects.none()
+
+        queryset = super().get_queryset().filter(schedules__organization=user_organization).distinct()
+        return queryset
+    
 
 class ManagerListViewAPI(generics.ListCreateAPIView):
     queryset = Manager.objects.all()
     serializer_class = ManagerSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return Manager.objects.none()
+
+        user_organization = user.organization
+        if not user_organization:
+            return Manager.objects.none()
+
+        queryset = super().get_queryset().filter(created_schedules__organization=user_organization).distinct()
+        return queryset
