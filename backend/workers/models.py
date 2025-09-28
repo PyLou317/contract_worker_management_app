4f7ee3ae95c000aa772ad128f46c7178a43fc995
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-27 21:51:25 -0700

    Unique together email and organization

--- backend/workers/models.py
+++ backend/workers/models.py
@@ -13,7 +13,7 @@ WORKER_POSITION = [
 class ContractWorker(models.Model):
     first_name = models.CharField(max_length=125)
     last_name = models.CharField(max_length=125)
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
     phone_number = models.CharField(max_length=20)
     current_contract = models.ForeignKey(WarehouseBusiness, on_delete=models.CASCADE)
     agency = models.ForeignKey(StaffingAgency, on_delete=models.CASCADE)
@@ -21,6 +21,12 @@ class ContractWorker(models.Model):
     
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
+    
+    class Meta:
+        verbose_name = _('Worker')
+        verbose_name_plural = _('Workers')
+        unique_together = (('email', 'current_contract'),) 
+        
     
 
 class Skill(models.Model):
