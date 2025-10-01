9fb92ef5494957e0b53873ed8e08349b89ee402b
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-09-30 19:30:43 -0700

    Fix Bug: shifts displayed correctly in schedule serializer

--- backend/scheduling/serializers.py
+++ backend/scheduling/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from .models import *
 
+
+class ShiftSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shift
+        fields = '__all__'
+        depth = 2
+        
+        
 class SchedulingSerializer(serializers.ModelSerializer):
     start_date = serializers.DateField(format='%Y-%m-%d')
     end_date = serializers.DateField(format='%Y-%m-%d')
+    shifts = ShiftSerializer(many=True, read_only=True)
     
     class Meta:
         model = Schedule
@@ -14,17 +23,11 @@ class SchedulingSerializer(serializers.ModelSerializer):
             'area', 
             'start_date', 
             'end_date', 
-            'is_published'
+            'is_published', 
+            'shifts'
         ]
-        depth = 1
-        
-
-class ShiftSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Shift
-        fields = '__all__'
         depth = 2
-
+        
 
 class AreaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -39,10 +42,38 @@ class ManagerSerializer(serializers.ModelSerializer):
         
         
 class CreateScheduleSerializer(serializers.ModelSerializer):
-    shift = ShiftSerializer(many=True, read_only=True)
+    shifts = ShiftSerializer(many=True)
+    area = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Area.objects.all(),
+    )
+    manager = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Manager.objects.all(),
+    )
     
     class Meta:
         model = Schedule
-        fields = '__all__'
-        read_only_fields = ('organization', 'shifts') 
-        depth = 2
+        fields = (
+            'id', 
+            'organization', 
+            'manager', 
+            'area', 
+            'start_date', 
+            'end_date', 
+            'is_published', 
+            'shifts'
+        )
+        read_only_fields = ('organization', 'shift') 
+    
+    def create(self, validated_data):
+        shifts_data = validated_data.pop('shifts', [])
+
+        schedule = super().create(validated_data)         
+        
+        for shift_data in shifts_data:
+            if 'id' in shift_data:
+                shift_data.pop('id') 
+            Shift.objects.create(schedule=schedule, **shift_data)
+        
+        return schedule
