c9296d5e0748b7c7dcd1e05db66cb97961d140d2
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-10-06 22:48:21 -0700

    Fix Bug: Date is off by -1

--- frontend/src/utilities/formatDate.js
+++ frontend/src/utilities/formatDate.js
@@ -1,7 +1,7 @@
 export default function formatDate(rawDate) {
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const date = new Date(rawDate);
-  const day = date.getDate();
+  const day = (date.getDate() + 1);
   const month = months[date.getMonth()];
   const newDate = `${month} ${day}`;
   return newDate;
