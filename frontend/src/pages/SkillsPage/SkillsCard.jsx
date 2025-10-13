9fe752f017672970de81396e784b3c7b2e7cb189
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-10-13 12:17:20 -0700

    Make a full width element

--- frontend/src/pages/SkillsPage/SkillsCard.jsx
+++ frontend/src/pages/SkillsPage/SkillsCard.jsx
@@ -4,22 +4,27 @@ import './SkillCard.css';
 export default function SkillsCard({ skill, handleEditSkillClick }) {
   const colorKey = skill?.base_color || 'gray';
   const levelKey = String(skill?.level || 1);
-  const colorClass = skillColorClasses[colorKey]?.[levelKey] || skillColorClasses.gray[1];
+  const colorClass =
+    skillColorClasses[colorKey]?.[levelKey] || skillColorClasses.gray[1];
 
   return (
-    <div className="flex items-center h-[100px] gap-4 p-4 bg-white rounded-xl shadow-sm border border-gray-200">
+    <div className="flex items-center mb-2 p-4 bg-white rounded-xl shadow-sm border border-gray-200 hover:scale-101 transition-transform duration-200">
       <div
-        className={`h-10 w-10 rounded-full ${colorClass} text-white flex items-center justify-center font-bold text-sm`}
+        className={`h-12 w-12 me-4 rounded-full ${colorClass} text-white flex items-center justify-center font-bold text-sm`}
       >
         {skill.abreviation}
       </div>
       <div className="truncate">
-        <h3 className="font-semibold text-gray-900 md:font-sm">{skill.skill_name}</h3>
-        <p className="text-sm text-gray-500 overflow-hidden text-ellipsis">{skill.description}</p>
+        <h3 className="font-semibold text-gray-900 md:font-sm">
+          {skill.skill_name}
+        </h3>
+        <p className="text-sm text-gray-500 overflow-hidden text-ellipsis">
+          {skill.description}
+        </p>
       </div>
       <button
         onClick={(event) => handleEditSkillClick(event)}
-        className="text-xl font-bold text-gray-500 hover:text-gray-700 hover:cursor-pointer transition-colors duration-200 ms-auto mt-auto"
+        className="text-xl font-bold text-gray-500 hover:text-gray-700 hover:cursor-pointer transition-colors duration-200 ms-auto"
       >
         ...
       </button>
