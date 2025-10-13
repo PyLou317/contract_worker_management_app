82df9a27f5a6bce5ed0ec87fa4b23f75793798e2
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-10-13 12:11:40 -0700

    Add hover effect

--- frontend/src/pages/Dashboard/StatCard.jsx
+++ frontend/src/pages/Dashboard/StatCard.jsx
@@ -2,11 +2,17 @@ import LoadingSpinner from '../../components/Loader';
 
 export default function StatCard({ icon, title, value, className, loading }) {
   return (
-    <div className={`py-8 px-4 bg-white rounded-lg shadow-md flex items-center space-x-4 ${className}`}>
+    <div
+      className={`py-8 px-4 bg-white rounded-lg shadow-md flex items-center space-x-4 hover:scale-101 transition-transform duration-200 ${className}`}
+    >
       <div className="flex-shrink-0 bg-blue-100 p-2 rounded-full">{icon}</div>
       <div>
         <h4 className="text-gray-500 text-sm font-medium">{title}</h4>
-              {loading ? <LoadingSpinner size="10"/> : <p className="text-3xl font-semibold text-gray-900">{value}</p>}
+        {loading ? (
+          <LoadingSpinner size="10" />
+        ) : (
+          <p className="text-3xl font-semibold text-gray-900">{value}</p>
+        )}
       </div>
     </div>
   );
