e003a74ccb9e71e5ad131ebbe72c94bb53146351
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-10-13 10:01:22 -0700

    Capitalize first letter input

--- frontend/src/pages/WorkerListPage/AddWorkerModal/AddWorkerModal.jsx
+++ frontend/src/pages/WorkerListPage/AddWorkerModal/AddWorkerModal.jsx
@@ -1,9 +1,13 @@
 import { useState, useRef } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+
+import CapitalizeFirstLetter from '@/utilities/capitalizeFirstLetter';
+
 import Input from '@/components/Inputs/LabeledInput';
 import SelectInput from '@/components/Inputs/LabeledSelectInput';
 import CancelBtn from '@/components/Buttons/CancelBtn';
 import SubmitBtn from '@/components/Buttons/SubmitBtn';
+import Modal from '@/components/Modals/Modal';
 
 import { apiFetch } from '@/utilities/apiClient';
 import addWorker from '@/hooks/addWorker';
@@ -67,89 +71,114 @@ export default function AddWorkerModal({ showModal, onClose, editingWorker }) {
   const inputLabelClasses = 'block text-sm font-medium text-gray-200';
 
   return (
-    <div
-      className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${
-        showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'
-      }`}
-    >
-      <div className="bg-gray-800/75 absolute inset-0" onClick={onClose}></div>
-      <dialog
-        ref={dialogRef}
-        className={` bg-gray-900 text-white p-8 rounded-2xl shadow-xl max-w-lg w-full transform transition-transform duration-300 relative ${
-          showModal ? 'scale-100' : 'scale-95'
-        }`}
-        open={showModal}
-        onClose={onClose}
-      >
-        <div className="flex justify-between items-center pb-3">
-          <h3 className="text-2xl font-semibold text-white">Add New Worker</h3>
-          <button onClick={onClose} className="text-gray-200 hover:text-gray-700">
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <Input
-            type="text"
-            id="first_name"
-            name="first_name"
-            label="First Name"
-            value={formData.first_name}
-            onChange={handleInputChange}
-            className={inputFieldClasses}
-            labelClasses={inputLabelClasses}
-          />
-          <Input
-            type="text"
-            id="last_name"
-            name="last_name"
-            label="Last Name"
-            value={formData.last_name}
-            onChange={handleInputChange}
-            className={inputFieldClasses}
-            labelClasses={inputLabelClasses}
-          />
-          <Input
-            type="email"
-            id="email"
-            name="email"
-            label="Email"
-            value={formData.email}
-            onChange={handleInputChange}
-            className={inputFieldClasses}
-          />
-          <Input
-            type="text"
-            id="phone_number"
-            name="phone_number"
-            label="Phone Number"
-            value={formData.phone_number}
-            onChange={handleInputChange}
-            className={inputFieldClasses}
-          />
-          {agencies && (
-            <SelectInput
-              label="Agency"
-              type="text"
-              id="agency"
-              name="agency"
-              value={formData.agency}
-              onChange={handleInputChange}
-              required
-              options={agencyNames}
-              className={inputFieldClasses}
-            ></SelectInput>
-          )}
-          <div className="mt-6 flex justify-end gap-3">
-            <CancelBtn onClick={onClose} disabled={addWorkerMutation.isPending} label="Cancel" />
-            <SubmitBtn
-              label={addWorkerMutation.isPending ? 'Adding...' : editingWorker ? 'Save Changes' : 'Add Worker'}
-              disabled={addWorkerMutation.isPending}
+    //     <div
+    //     className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${
+    //         showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'
+    //     }`}
+    //     >
+    //   <div className="bg-gray-800/75 absolute inset-0" onClick={onClose}></div>
+    //   <dialog
+    //     ref={dialogRef}
+    //     className={` bg-gray-900 text-white p-8 rounded-2xl shadow-xl max-w-lg w-full transform transition-transform duration-300 relative ${
+    //         showModal ? 'scale-100' : 'scale-95'
+    //     }`}
+    //     open={showModal}
+    //     onClose={onClose}
+    //   >
+    <Modal show={showModal} onClose={onClose}>
+      <div className="flex justify-between items-center pb-3">
+        <h3 className="text-2xl font-semibold text-white">Add New Worker</h3>
+        <button onClick={onClose} className="text-gray-200 hover:text-gray-700">
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
             />
-          </div>
-        </form>
-      </dialog>
-    </div>
+          </svg>
+        </button>
+      </div>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <Input
+          type="text"
+          id="first_name"
+          name="first_name"
+          label="First Name"
+          value={CapitalizeFirstLetter(formData.first_name)}
+          onChange={handleInputChange}
+          className={inputFieldClasses}
+          labelClasses={inputLabelClasses}
+        />
+        <Input
+          type="text"
+          id="last_name"
+          name="last_name"
+          label="Last Name"
+          value={CapitalizeFirstLetter(formData.last_name)}
+          onChange={handleInputChange}
+          className={inputFieldClasses}
+          labelClasses={inputLabelClasses}
+        />
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          label="Email"
+          value={formData.email}
+          onChange={handleInputChange}
+          className={inputFieldClasses}
+        />
+        <Input
+          type="text"
+          id="phone_number"
+          name="phone_number"
+          label="Phone Number"
+          value={formData.phone_number}
+          onChange={handleInputChange}
+          className={inputFieldClasses}
+        />
+        {agencies && (
+          <SelectInput
+            label="Agency"
+            type="text"
+            id="agency"
+            name="agency"
+            value={formData.agency}
+            onChange={handleInputChange}
+            required
+            options={agencyNames}
+            className={inputFieldClasses}
+          ></SelectInput>
+        )}
+        <div className="mt-6 flex justify-end gap-3">
+          <CancelBtn
+            onClick={onClose}
+            disabled={addWorkerMutation.isPending}
+            label="Cancel"
+          />
+          <SubmitBtn
+            label={
+              addWorkerMutation.isPending
+                ? 'Adding...'
+                : editingWorker
+                ? 'Save Changes'
+                : 'Add Worker'
+            }
+            disabled={addWorkerMutation.isPending}
+          />
+        </div>
+      </form>
+    </Modal>
   );
+}
+
+{
+  /* </dialog>
+</div> */
 }
