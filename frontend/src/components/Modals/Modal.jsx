1da18dbe55576b2d54f5b0298c874514d29cc00a
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-10-13 20:28:07 -0700

    Change modal div to dialog to add more built in features

--- frontend/src/components/Modals/Modal.jsx
+++ frontend/src/components/Modals/Modal.jsx
@@ -1,15 +1,25 @@
-import handleLogout from '../Authentication/LogoutFunction';
+import { useRef } from 'react';
 
-export default function WarningModal({ show, modalTitle, children }) {
+export default function Modal({ show, children, onClose }) {
+  const dialogRef = useRef(null);
+
   if (!show) {
     return null;
   }
 
   return (
-    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-      <div className="w-full max-w-md py-12 bg-gray-800 rounded-lg shadow-xl">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-gray-800/75 absolute inset-0" onClick={onClose}></div>
+      <dialog
+        ref={dialogRef}
+        className={` bg-gray-900 text-white p-8 rounded-2xl shadow-xl max-w-lg w-full transform transition-transform duration-300 relative ${
+          show ? 'scale-100' : 'scale-95'
+        }`}
+        open={show}
+        onClose={onClose}
+      >
         {children}
-      </div>
+      </dialog>
     </div>
   );
 }
