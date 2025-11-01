a88dc29466d0d6c0f7d8279c2eb03c26a02702dd
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-10-31 21:39:06 -0700

    add logic for displaying brand name

--- frontend/src/components/Brand.jsx
+++ frontend/src/components/Brand.jsx
@@ -1,11 +1,18 @@
-export default function BrandLogoAndName({ className }) {
+export default function BrandLogoAndName({ className, sidebarOpen }) {
+  let brandName = '';
+  if (!sidebarOpen) {
+    brandName = '';
+  } else {
+    brandName = 'the HIVE';
+  }
+
   return (
     <>
       <h1 className={className}>
         <span className="text-yellow-100">
           <i className="fa-solid fa-hexagon"></i>
         </span>{' '}
-        the HIVE
+        <span>{brandName}</span>
       </h1>
     </>
   );
