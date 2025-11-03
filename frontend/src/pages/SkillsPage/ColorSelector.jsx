a2c0883298f7bae12be9d1093fdb418f43439cff
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 20:45:09 -0800

    Move capitaliz first letter function to separate file. Reformat for floating labels

--- frontend/src/pages/SkillsPage/ColorSelector.jsx
+++ frontend/src/pages/SkillsPage/ColorSelector.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import SelectInput from '@/components/Inputs/FloatingSelectInput';
 import skillColorClasses from './SkillColorClasses';
 
 const colors = Object.keys(skillColorClasses);
 
-export default function ColorSelector({ onColorChange, extraClasses }) {
+export default function ColorSelector({ onColorChange }) {
   const [selectedColor, setSelectedColor] = useState(colors[0]);
 
   const handleColorChange = (e) => {
@@ -11,39 +12,22 @@ export default function ColorSelector({ onColorChange, extraClasses }) {
     onColorChange(e.target.value);
   };
 
-  function capitalizeFirstLetter(word) {
-    if (typeof word !== 'string' || word.length === 0) {
-      return '';
-    }
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
-
   return (
-    <div className="flex flex-row space-y-4 gap-4">
-      <div>
-        <label className="block text-gray-700 text-xs font-bold mb-2" htmlFor="color">
-          Base Color
-        </label>
-        <select
-          className={`border rounded-lg p-2 w-full h-[40px] text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 ${extraClasses}`}
-          id="color"
-          name="color"
-          value={selectedColor}
-          onChange={handleColorChange}
-          required
-        >
-          {colors.map((color) => (
-            <option key={color} value={color}>
-              {capitalizeFirstLetter(color)}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="flex justify-center items-center mt-2">
-        <div
-          className={`h-10 w-10 rounded-lg border border-gray-300 shadow-lg transition-all duration-300 ${skillColorClasses[selectedColor][1]}`}
-        ></div>
-      </div>
+    <div className="flex flex-row items-center gap-4 w-full">
+      <SelectInput
+        label="Color"
+        type="text"
+        id="color"
+        name="color"
+        value={selectedColor}
+        onChange={handleColorChange}
+        options={colors}
+        required
+        className="flex-grow-1"
+      />
+      <div
+        className={`h-10 w-10 rounded-lg border border-gray-300 shadow-lg transition-all duration-300 ${skillColorClasses[selectedColor][1]}`}
+      ></div>
     </div>
   );
 }
