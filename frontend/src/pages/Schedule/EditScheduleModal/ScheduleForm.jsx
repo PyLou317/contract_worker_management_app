bc2f15f35acb5123cacc825797cc1b9c19d08141
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 20:43:02 -0800

    Add activate schedule toggle

--- frontend/src/pages/Schedule/EditScheduleModal/ScheduleForm.jsx
+++ frontend/src/pages/Schedule/EditScheduleModal/ScheduleForm.jsx
@@ -3,14 +3,13 @@ import { EditScheduleContext } from './edit-schedule-context';
 import { ScheduleContext } from '../schedule-page-context';
 
 import Input from '@/components/Inputs/LabeledInput';
-import SelectInput from '@/components/Inputs/LabeledSelectInput';
+import SelectInput from '@/components/Inputs/FloatingSelectInput';
 
 export default function ScheduleForm() {
-    const { managers, areas } = useContext(ScheduleContext);
+  const { managers, areas } = useContext(ScheduleContext);
 
   const { formData, handleScheduleInputChange } =
-      useContext(EditScheduleContext);
-
+    useContext(EditScheduleContext);
 
   const inputClasses =
     'flex flex-grow-1 border border-gray-300 rounded-lg p-2 w-full text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500';
@@ -18,8 +17,28 @@ export default function ScheduleForm() {
 
   return (
     <>
-      <h1 className="text-xl font-semibold text-gray-800 mb-2">Schedule</h1>
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:items-end border border-gray-300 rounded-xl shadow p-4 mb-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-xl font-semibold text-gray-800">Schedule</h1>
+        <label
+          htmlFor="is_published"
+          className="inline-flex items-center cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            id="is_published"
+            name="is_published"
+            className="sr-only peer"
+            onChange={handleScheduleInputChange}
+            checked={formData.is_published}
+            value={formData.is_published}
+          />
+          <div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-400 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
+          <span className="ms-3 text-sm font-medium text-gray-700">
+            Activate Schedule
+          </span>
+        </label>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:items-end border border-gray-300 rounded-xlƒ shadow p-4 mb-4">
         <SelectInput
           label="Manager"
           name="manager"
