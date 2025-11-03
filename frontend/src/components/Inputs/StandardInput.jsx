d541e52e6a5d7c30eb3f985d1a671598e4d5fce9
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 20:21:58 -0800

    Pass placeholder for floating label

--- frontend/src/components/Inputs/StandardInput.jsx
+++ frontend/src/components/Inputs/StandardInput.jsx
@@ -1,14 +1,12 @@
 export default function Input(props) {
   return (
-    <div>
-      <input
-        {...props}
-        className={`
-        mt-1 p-2 block w-full rounded-md shadow-sm
-        focus:ring-blue-500 focus:border-blue-500
+    <input
+      {...props}
+      className={`
+        peer p-2 block w-full placeholder-transparent caret-gray-800 text-gray-800 border-b-2 border-b-gray-500 focus:border-yellow-500 focus:outline-none
         ${props.className || ''}
       `}
-      />
-    </div>
+      placeholder={props.placeholder}
+    />
   );
 }
