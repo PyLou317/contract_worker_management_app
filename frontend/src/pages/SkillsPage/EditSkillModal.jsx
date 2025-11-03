4664e16c6cdc84bc6191b0b276964e11e0ea5af2
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 20:33:45 -0800

    Render selected skill data and format inputs with floating labels

--- frontend/src/pages/SkillsPage/EditSkillModal.jsx
+++ frontend/src/pages/SkillsPage/EditSkillModal.jsx
@@ -1,18 +1,29 @@
-import { useContext, useRef } from 'react';
+import { useEffect, useContext, useRef, useState } from 'react';
 import { SkillsContext } from './skills-page-ctx';
 import { useQuery } from '@tanstack/react-query';
+import { apiFetch } from '@/utilities/apiClient';
 
 import Input from '@/components/Inputs/LabeledInput';
-import SelectInput from '@/components/Inputs/LabeledSelectInput';
+import SelectInput from '@/components/Inputs/FloatingSelectInput';
 import CancelBtn from '@/components/Buttons/CancelBtn';
 import SubmitBtn from '@/components/Buttons/SubmitBtn';
 import ColorSelector from './ColorSelector';
+import skillColorClasses from './SkillColorClasses';
+
+const colors = Object.keys(skillColorClasses);
 
 export default function EditSkillModal({ skillId }) {
-  const { showEditSkillModal, onClose, handleSubmit, formData } = useContext(SkillsContext);
+  const { showEditSkillModal, onClose, handleSubmit } =
+    useContext(SkillsContext);
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
   const dialogRef = useRef(null);
 
-  const skillNames = ['Skill 1', 'Skill 2', 'Skill 3'];
+  const [formData, setFormData] = useState({
+    skill_name: '',
+    abreviation: '',
+    base_color: '',
+    description: '',
+  });
 
   const {
     data: skillData,
@@ -20,9 +31,22 @@ export default function EditSkillModal({ skillId }) {
     error: skillError,
   } = useQuery({
     queryKey: ['skill', skillId],
-    queryFn: () => getWorkerDetails({ skillId: skillId }),
-    enabled: !!skillId, // Only run this query if an ID exists
+    queryFn: () => apiFetch(`/skills/${skillId}`),
+    enabled: !!skillId,
   });
+
+  useEffect(() => {
+    if (skillData) {
+      setFormData({
+        skill_name: skillData.skill_name,
+        abreviation: skillData.abreviation,
+        base_color: skillData.base_color,
+        description: skillData.description,
+      });
+
+      setSelectedColor(skillData.base_color);
+    }
+  }, [skillData]);
 
   function getAcronym(str) {
     if (!str) {
@@ -40,6 +64,7 @@ export default function EditSkillModal({ skillId }) {
 
     // Temporary variable to hold the updated data
     let newData = { ...formData, [name]: value };
+    console.log(newData);
 
     // If skill_name changes, update the acronym
     if (name === 'skill_name') {
@@ -48,10 +73,11 @@ export default function EditSkillModal({ skillId }) {
     setFormData(newData);
   };
 
-  const inputFieldClasses =
-    'mt-1 p-2 block w-full rounded-md border bg-gray-800 border-gray-400 shadow-sm focus:border-blue-500 focus:ring-blue-500';
-  const inputLabelClasses = 'block text-sm font-medium text-gray-200';
-  const selectInputClasses = 'bg-gray-800 text-gray-200 border border-gray-400 text-gray-200';
+  const handleColorSelectChange = (e) => {
+    const color = e.target.value;
+    setSelectedColor(color);
+    setFormData({ ...formData, base_color: color });
+  };
 
   return (
     <div
@@ -62,54 +88,90 @@ export default function EditSkillModal({ skillId }) {
       <div className="bg-gray-800/75 absolute inset-0" onClick={onClose}></div>
       <dialog
         ref={dialogRef}
-        className={` bg-gray-900 text-white p-8 rounded-2xl shadow-xl max-w-lg w-full transform transition-transform duration-300 relative ${
+        className={` bg-white text-white p-8 rounded-2xl shadow-xl max-w-lg w-full transform transition-transform duration-300 relative ${
           showEditSkillModal ? 'scale-100' : 'scale-95'
         }`}
         open={showEditSkillModal}
         onClose={onClose}
       >
         <div className="flex justify-between items-center pb-3">
-          <h3 className="text-2xl font-semibold text-white">Edit Skill</h3>
-          <button onClick={onClose} className="text-gray-200 hover:text-gray-700">
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+          <h3 className="text-2xl font-semibold text-gray-800">Edit Skill</h3>
+          <button
+            onClick={onClose}
+            className="text-gray-400 hover:text-gray-600"
+          >
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </button>
         </div>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <SelectInput
+        <form onSubmit={handleSubmit} className="space-y-8 mt-8">
+          <Input
             label="Skill Name"
             type="text"
             id="skill_name"
             name="skill_name"
-            value={''}
+            placeholder="Skill Name"
+            value={formData.skill_name}
             onChange={handleInputChange}
-            options={skillNames}
-            className={selectInputClasses}
           />
           <Input
+            label="Abreviation"
             type="text"
             id="abreviation"
             name="abreviation"
-            label="Abreviation"
-            value={''}
+            placeholder="Abreviation"
+            value={formData.abreviation}
             onChange={handleInputChange}
-            className={inputFieldClasses}
-            labelClasses={inputLabelClasses}
           />
-          <Input
-            type="text"
-            id="description"
-            name="description"
-            label="Description"
-            value={''}
-            onChange={handleInputChange}
-            className={inputFieldClasses}
-          />
-          <ColorSelector
-            extraClasses="bg-gray-800 border-gray-400 text-white"
-            onColorChange={(color) => setFormData({ ...formData, base_color: color })}
-          />
+          <div className="flex flex-row items-center gap-4 w-full">
+            <SelectInput
+              label="Color"
+              type="text"
+              id="base_color"
+              name="base_color"
+              value={formData.base_color}
+              onChange={handleColorSelectChange}
+              options={colors}
+              required
+              className="flex-grow-1"
+            />
+            <div
+              className={`h-10 w-10 rounded-lg border border-gray-300 shadow-lg transition-all duration-300 ${skillColorClasses[selectedColor][1]}`}
+            ></div>
+          </div>
+          {/* <ColorSelector
+            onColorChange={(color) =>
+              setFormData({ ...formData, base_color: color })
+            }
+          /> */}
+          <div className="relative">
+            <textarea
+              id="description"
+              name="description"
+              rows={1}
+              value={formData.description}
+              placeholder="Description"
+              onChange={handleInputChange}
+              className="peer p-2 block w-full placeholder-transparent bg-white rounded-t-lg caret-gray-800 text-gray-800 border-b-2 border-b-gray-600 focus:border-yellow-500 focus:outline-none mt-12"
+            ></textarea>
+            <label
+              htmlFor="comment"
+              className="peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 absolute text-gray-500 left-0 -top-5.5 text-sm transition-all cursor-text"
+            >
+              Description
+            </label>
+          </div>
           <div className="mt-6 flex justify-end gap-3">
             <CancelBtn onClick={onClose} label="Cancel" />
             <SubmitBtn
