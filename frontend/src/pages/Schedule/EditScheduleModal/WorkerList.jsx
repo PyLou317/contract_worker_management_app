4b826443d4f24a228d890a370e448a09ec9571af
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 21:01:51 -0800

    Add pagination and search and ordering params

--- frontend/src/pages/Schedule/EditScheduleModal/WorkerList.jsx
+++ frontend/src/pages/Schedule/EditScheduleModal/WorkerList.jsx
@@ -6,23 +6,39 @@ import { apiFetch } from '@/utilities/apiClient';
 
 import '@/utilities/toolTipStyles.css';
 
+import Pagination from '@/components/Pagination';
+import Search from '@/components/Search';
 import skillColorClasses from '@/pages/SkillsPage/SkillColorClasses';
 import LoadingSpinner from '@/components/Loader';
 
-export default function WorkerList({ shiftId }) {
+export default function WorkerList({
+  shiftId,
+  index,
+  handleAddWorkerClick,
+  addWorkersId,
+}) {
   const [ordering, setOrdering] = useState('');
   const [hoveredWorkerId, setHoveredWorkerId] = useState(null);
   const [page, setPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
 
-  const { formData, handleWorkerCheck } = useContext(EditScheduleContext);
+  const { shiftsData, formData, handleWorkerCheck } =
+    useContext(EditScheduleContext);
+
+  const targetShift = shiftsData?.find((shift) => shift.id === shiftId);
+  const targetShiftWorkers = targetShift?.workers || [];
 
   const starRating = {
-    size: 24,
+    size: 20,
     allowFraction: true,
     readonly: true,
     allowHover: false,
     fillColor: '#ffd700',
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+    setPage(1);
   };
 
   const handleRatingSort = () => {
@@ -34,14 +50,14 @@ export default function WorkerList({ shiftId }) {
     setPage(1);
   };
 
-  //   const updateUrl = (newPage) => {
-  //     const params = new URLSearchParams(window.location.search);
-  //     params.set('page', newPage);
-  //     window.history.pushState(null, '', `?${params.toString()}`);
-  //   };
+  const updateUrl = (newPage) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set('page', newPage);
+    window.history.pushState(null, '', `?${params.toString()}`);
+  };
 
   const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['workers'],
+    queryKey: ['workers', searchTerm, page, ordering],
     queryFn: () => {
       const params = new URLSearchParams();
       if (page > 1) {
@@ -77,6 +93,7 @@ export default function WorkerList({ shiftId }) {
   const shiftIndex = formData.shifts.findIndex((shift) => shift.id === shiftId);
   const currentShiftWorkers =
     formData.shifts[shiftIndex]?.contract_workers || [];
+
   const selectedWorkerIds = currentShiftWorkers.map(
     (workerObj) => workerObj.id
   );
@@ -151,140 +168,154 @@ export default function WorkerList({ shiftId }) {
         <>
           <Search onSearch={handleSearch} searchTerm={searchTerm} />
           <div className="overflow-x-auto border border-gray-300 rounded-lg mb-8">
-      <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
-        <thead className="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
-          <tr>
-            <th className="py-1 px-6 text-left border-r border-gray-300">
-              <input
-                type="checkbox"
-                id="schedule_worker"
-                name="schedule_worker"
-                value="yes"
-              />
-            </th>
-            <th className="py-1 px-6 text-left border-r border-gray-300">
-              Name
-            </th>
-            <th className="py-1 px-6 text-left border-r border-gray-300">
-              Position
-            </th>
-            <th className="py-1 px-6 text-left border-r border-gray-300">
-              Agency
-            </th>
-            <th className="py-1 px-6 text-left border-r border-gray-300">
-              <div className="flex justify-between items-center">
-                Rating{' '}
-                <button
-                  onClick={handleRatingSort}
-                  className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-6 cursor-pointer"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+            <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
+              <thead className="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
+                <tr>
+                  <th className="py-1 px-6 text-left border-r border-gray-300">
+                    <input
+                      type="checkbox"
+                      id="schedule_worker"
+                      name="schedule_worker"
+                      value="yes"
                     />
-                  </svg>
-                </button>
-              </div>
-            </th>
-            <th className="py-3 px-6 text-left">Skills</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          {workers && workers.length > 0 ? (
-            workers.map((worker, index) => (
-              <tr
-                key={worker.id}
-                className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-200"
-              >
-                <td className="py-3 px-6 text-left whitespace-nowrap border-r border-gray-200">
-                  <input
-                    type="checkbox"
-                    id={`contract_workers_${worker.id}`}
-                    name="contract_workers"
-                    value={worker.id}
-                    checked={selectedWorkerIds.includes(worker.id)}
-                    onChange={(e) =>
-                      handleWorkerCheck(
-                        shiftIndex,
-                        e.target.value,
-                        e.target.checked
-                      )
-                    }
-                  />
-                </td>
-                <td className="py-3 px-6 text-left border-r border-gray-200">
-                  {worker.first_name} {worker.last_name}
-                </td>
-                <td className="py-3 px-6 text-left border-r border-gray-200">
-                  {worker.position}
-                </td>
-                <td className="py-3 px-6 text-left border-r border-gray-200">
-                  {worker.agency_details}
-                </td>
-                <td
-                  className="py-3 px-6 text-left border-r border-gray-200 relative"
-                  onMouseEnter={() => setHoveredWorkerId(worker.id)}
-                  onMouseLeave={() => setHoveredWorkerId(null)}
-                >
-                  {hoveredWorkerId === worker.id && (
-                    <div className="absolute bottom-1/2 left-1/2 -translate-x-1/2 mb-2 bg-white border border-gray-300 p-2 rounded shadow-lg text-sm z-10 whitespace-nowrap">
-                      {worker.rating ? worker.rating.average_rating : 'N/A'} out
-                      of 5
-                    </div>
-                  )}
-                  <Rating
-                    initialValue={
-                      worker.rating !== null && worker.rating.average_rating
-                        ? worker.rating.average_rating
-                        : 0
-                    }
-                    {...starRating}
-                  />
-                </td>
-                <td className="text-white py-3 px-6 text-left border-r border-gray-200">
-                  <div className="flex flex-wrap gap-2">
-                    {worker.worker_skills !== null &&
-                      worker.worker_skills?.map((skill, index) => (
-                        <span
-                          className={`text-white text-xs font-semibold px-2.5 py-1 rounded-full shadow-md transition-colors duration-200 tooltip ${
-                            skillColorClasses[skill.skill.base_color]?.[
-                              skill.level
-                            ] || 'bg-gray-400 hover:bg-gray-500'
-                          }`}
-                          key={index}
+                  </th>
+                  <th className="py-1 px-6 text-left border-r border-gray-300">
+                    Name
+                  </th>
+                  <th className="py-1 px-6 text-left border-r border-gray-300">
+                    Position
+                  </th>
+                  <th className="py-1 px-6 text-left border-r border-gray-300">
+                    Agency
+                  </th>
+                  <th className="py-1 px-6 text-left border-r border-gray-300">
+                    <div className="flex justify-between items-center">
+                      Rating{' '}
+                      <button
+                        onClick={handleRatingSort}
+                        className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="size-6 cursor-pointer"
                         >
-                          {skill.skill.abreviation}LV{skill.level}
-                          <div className="tooltip-content cursor-pointer">
-                            {skill.skill.skill_name} level {skill.level}
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </th>
+                  <th className="py-3 px-6 text-left">Skills</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-600 text-sm font-light">
+                {unscheduledWorkers && unscheduledWorkers.length > 0 ? (
+                  unscheduledWorkers.map((worker, index) => (
+                    <tr
+                      key={worker.id}
+                      className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-200"
+                    >
+                      <td className="py-3 px-6 text-left whitespace-nowrap border-r border-gray-200">
+                        <input
+                          type="checkbox"
+                          id={`contract_workers_${worker.id}`}
+                          name="contract_workers"
+                          value={worker.id}
+                          checked={selectedWorkerIds.includes(worker.id)}
+                          onChange={(e) =>
+                            handleWorkerCheck(
+                              shiftIndex,
+                              e.target.value,
+                              e.target.checked
+                            )
+                          }
+                        />
+                      </td>
+                      <td className="py-3 px-6 text-left border-r border-gray-200">
+                        {worker.first_name} {worker.last_name}
+                      </td>
+                      <td className="py-3 px-6 text-left border-r border-gray-200">
+                        {worker.position}
+                      </td>
+                      <td className="py-3 px-6 text-left border-r border-gray-200">
+                        {worker.agency_details}
+                      </td>
+                      <td
+                        className="py-3 px-6 text-left border-r border-gray-200 relative"
+                        onMouseEnter={() => setHoveredWorkerId(worker.id)}
+                        onMouseLeave={() => setHoveredWorkerId(null)}
+                      >
+                        {hoveredWorkerId === worker.id && (
+                          <div className="absolute bottom-1/2 left-1/2 -translate-x-1/2 mb-2 bg-white border border-gray-300 p-2 rounded shadow-lg text-sm z-10 whitespace-nowrap">
+                            {worker.rating
+                              ? worker.rating.average_rating
+                              : 'N/A'}{' '}
+                            out of 5
                           </div>
-                        </span>
-                      ))}
-                  </div>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="7" className="text-center">
-                <div className="flex justify-center items-center h-[100px]">
-                  <div className="flex justify-center items-center h-auto w-fit mx-auto bg-yellow-100 text-yellow-700 p-4 rounded-lg">
-                    No workers found, please add some.
-                  </div>
-                </div>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+                        )}
+                        <Rating
+                          initialValue={
+                            worker.rating !== null &&
+                            worker.rating.average_rating
+                              ? worker.rating.average_rating
+                              : 0
+                          }
+                          {...starRating}
+                        />
+                      </td>
+                      <td className="text-white py-3 px-6 text-left border-r border-gray-200">
+                        <div className="flex flex-wrap gap-2">
+                          {worker.worker_skills !== null &&
+                            worker.worker_skills?.map((skill, index) => (
+                              <span
+                                className={`text-white text-xs font-semibold px-2.5 py-1 rounded-full shadow-md transition-colors duration-200 tooltip ${
+                                  skillColorClasses[skill.skill.base_color]?.[
+                                    skill.level
+                                  ] || 'bg-gray-400 hover:bg-gray-500'
+                                }`}
+                                key={index}
+                              >
+                                {skill.skill.abreviation}LV{skill.level}
+                                <div className="tooltip-content cursor-pointer">
+                                  {skill.skill.skill_name} level {skill.level}
+                                </div>
+                              </span>
+                            ))}
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="7" className="text-center">
+                      <div className="flex justify-center items-center h-[100px]">
+                        <div className="flex justify-center items-center h-auto w-fit mx-auto bg-yellow-100 text-yellow-700 p-4 rounded-lg">
+                          No workers found, please add some.
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+          <Pagination
+            page={page}
+            setPage={setPage}
+            totalPages={data?.count}
+            nextUrl={nextUrl}
+            prevUrl={prevUrl}
+            updateUrl={updateUrl}
+          />
+        </>
+      )}
+    </>
   );
 }
