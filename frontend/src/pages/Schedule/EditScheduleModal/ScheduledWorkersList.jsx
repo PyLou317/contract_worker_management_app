93940844682f4afe801c18ae29f426115903706a
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 21:02:27 -0800

    Add search and sendSMS logic

--- frontend/src/pages/Schedule/EditScheduleModal/ScheduledWorkersList.jsx
+++ frontend/src/pages/Schedule/EditScheduleModal/ScheduledWorkersList.jsx
@@ -1,34 +1,78 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { Rating } from 'react-simple-star-rating';
 import { EditScheduleContext } from './edit-schedule-context';
-import { useQuery } from '@tanstack/react-query';
-import { apiFetch } from '@/utilities/apiClient';
+import { useSendSMSMutation } from '@/hooks/sendSMSMutation';
 
 import '@/utilities/toolTipStyles.css';
 
 import Search from '@/components/Search';
 import skillColorClasses from '@/pages/SkillsPage/SkillColorClasses';
 import LoadingSpinner from '@/components/Loader';
+import ViewSchedWorkerBtn from '@/pages/Schedule/EditScheduleModal/ViewSchedWorkerBtn';
+import SMSNotificationModal from '@/pages/Schedule/EditScheduleModal/SendSMSNotificationModal';
 
-export default function ScheduledWorkersList({ shiftId, index }) {
+export default function ScheduledWorkersList({ shiftId }) {
   const [ordering, setOrdering] = useState('');
   const [hoveredWorkerId, setHoveredWorkerId] = useState(null);
   const [page, setPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const [scheduledWorkersView, setScheduledWorkersView] = useState(false);
+  const [displayWorkers, setDisplayWorkers] = useState([]);
+  const [openSMSModal, setOpenSMSModal] = useState(false);
 
   const { shiftsData, isPending, isFetching, error, handleWorkerCheck } =
     useContext(EditScheduleContext);
 
+  const setFormData = () => {};
+
   const targetShift = shiftsData.find((shift) => shift.id === shiftId);
   const targetShiftWorkers = targetShift?.workers || [];
 
+  useEffect(() => {
+    setDisplayWorkers(targetShiftWorkers);
+    setSearchTerm('');
+  }, [targetShiftWorkers]);
+
   const starRating = {
-    size: 24,
+    size: 20,
     allowFraction: true,
     readonly: true,
     allowHover: false,
     fillColor: '#ffd700',
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+
+    const lowerCaseTerm = term.toLowerCase();
+
+    if (!lowerCaseTerm.trim()) {
+      setDisplayWorkers(targetShiftWorkers);
+      return;
+    }
+
+    const filteredWorkers = targetShiftWorkers.filter((worker) => {
+      const firstName = worker.first_name
+        ? worker.first_name.toLowerCase()
+        : '';
+      const lastName = worker.last_name ? worker.last_name.toLowerCase() : '';
+      const agency = worker.agency_details
+        ? worker.agency_details.toLowerCase()
+        : '';
+      const position = worker.position ? worker.position.toLowerCase() : '';
+      // const skills = worker.worker_skills[0].skill?.skill_name ? worker.worker_skills[0].skill?.skill_name.toLowerCase() : '';
+      console.log('worker', worker);
+      console.log('Skills', skills);
+
+      return (
+        firstName.includes(lowerCaseTerm) ||
+        lastName.includes(lowerCaseTerm) ||
+        agency.includes(lowerCaseTerm) ||
+        position.includes(lowerCaseTerm)
+        // skills.includes(lowerCaseTerm)
+      );
+    });
+    setDisplayWorkers(filteredWorkers);
   };
 
   const handleRatingSort = () => {
@@ -40,14 +84,45 @@ export default function ScheduledWorkersList({ shiftId, index }) {
     setPage(1);
   };
 
-  //   const updateUrl = (newPage) => {
-  //     const params = new URLSearchParams(window.location.search);
-  //     params.set('page', newPage);
-  //     window.history.pushState(null, '', `?${params.toString()}`);
-  //   };
+  const updateUrl = (newPage) => {
+    const params = new URLSearchParams(window.location.search);
+    params.set('page', newPage);
+    window.history.pushState(null, '', `?${params.toString()}`);
+  };
 
   const toggleScheduledWorkersView = () => {
     setScheduledWorkersView(!scheduledWorkersView);
+  };
+
+  const handleOpenSendSMSModal = (e) => {
+    e.preventDefault();
+    setOpenSMSModal((prevOpenSMSModal) => !prevOpenSMSModal);
+  };
+
+  const { mutate: sendSMSMutation, isPending: isSendingSMS } =
+    useSendSMSMutation({ setFormData });
+
+  const sendSMS = (e) => {
+    e.preventDefault();
+
+    const messagesToSend = [];
+
+    targetShiftWorkers.forEach((element) => {
+      const workerName = `${element.first_name} ${element.last_name}`;
+      const message_body = `Hi ${workerName}, a new work schedule has been sent to your email.`;
+      const phone_number = element.phone_number;
+
+      const dataToSend = {
+        receipient_name: workerName,
+        message_body: message_body,
+        to_number: '+1' + phone_number,
+      };
+      messagesToSend.push(dataToSend);
+    });
+    messagesToSend.forEach((element) => {
+      sendSMSMutation(element);
+    });
+    setOpenSMSModal(false);
   };
 
   //   const scheduledworkers = formData.shifts[index].workers || [];
@@ -73,7 +148,8 @@ export default function ScheduledWorkersList({ shiftId, index }) {
     return (
       <div className="flex justify-center items-center h-[100px] my-8">
         <div className="flex justify-center items-center h-auto w-fit mx-auto bg-red-100 text-red-700 p-4 rounded-lg">
-          An error has occurred: {scheduleError.message}
+          An error has occurred: {scheduleError.message} An error has occurred:{' '}
+          {scheduleError.message}
         </div>
       </div>
     );
@@ -81,56 +157,21 @@ export default function ScheduledWorkersList({ shiftId, index }) {
 
   return (
     <>
-      <button
-        type="button"
-        onClick={toggleScheduledWorkersView}
-        className="flex gap-2 mt-4 items-center mb-2 cursor-pointer"
-      >
-        {scheduledWorkersView ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6 text-green-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6 text-green-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
-        )}
-        {scheduledWorkersView ? (
-          <h1 className="text-green-600">Close Scheduled Workers</h1>
-        ) : (
-          <h1 className="text-green-600">View Scheduled Workers </h1>
-        )}
-      </button>
+      <ViewSchedWorkerBtn
+        toggleScheduledWorkersView={toggleScheduledWorkersView}
+        scheduledWorkersView={scheduledWorkersView}
+      />
       {scheduledWorkersView && (
         <>
-          <Search />
+          <div className="flex justify-between align-center items-center mt-4 mb-2">
+            <Search onSearch={handleSearch} searchTerm={searchTerm} />
+            <button
+              className="bg-green-600 px-4 py-2 rounded-lg hover:bg-green-500 transition-colors duration-200 text-white font-medium"
+              onClick={handleOpenSendSMSModal}
+            >
+              {isSendingSMS ? 'Sending...' : 'Notify Workers'}
+            </button>
+          </div>
           <div className="overflow-x-auto border border-gray-300 rounded-lg mb-8">
             <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
               <thead className="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
@@ -181,7 +222,7 @@ export default function ScheduledWorkersList({ shiftId, index }) {
               </thead>
               <tbody className="text-gray-600 text-sm font-light">
                 {targetShiftWorkers && targetShiftWorkers.length > 0 ? (
-                  targetShiftWorkers.map((worker, index) => (
+                  displayWorkers.map((worker, index) => (
                     <tr
                       key={worker.id}
                       className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-200"
@@ -192,7 +233,7 @@ export default function ScheduledWorkersList({ shiftId, index }) {
                           id={`contract_workers_${worker.id}`}
                           name="contract_workers"
                           value={worker.id}
-                          checked={true}
+                          //   checked={''}
                           onChange={(e) =>
                             handleWorkerCheck(
                               index,
@@ -272,6 +313,12 @@ export default function ScheduledWorkersList({ shiftId, index }) {
           </div>
         </>
       )}
+      <SMSNotificationModal
+        show={openSMSModal}
+        onClose={handleOpenSendSMSModal}
+        workerCount={targetShiftWorkers.length}
+        sendSMS={sendSMS}
+      />
     </>
   );
 }
