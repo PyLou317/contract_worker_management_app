7ff170609d015f10e38d6fd0de53766afdccbe66
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 20:19:34 -0800

    Restructure for floating label

--- frontend/src/components/Inputs/LabeledInput.jsx
+++ frontend/src/components/Inputs/LabeledInput.jsx
@@ -2,14 +2,14 @@ import Input from './StandardInput';
 
 export default function LabeledInput({ label, id, labelClasses, ...props }) {
   return (
-    <div>
+    <div className="relative">
+      <Input id={id} {...props} />
       <label
         htmlFor={id}
-        className={labelClasses}
+        className={`peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 absolute text-gray-500 left-0 -top-4.5 text-sm transition-all cursor-text ${labelClasses}`}
       >
         {label}
       </label>
-      <Input id={id} {...props} />
     </div>
   );
 }
