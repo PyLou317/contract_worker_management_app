dfbace3f9a1e567efcaaedf0d8e4592af085e0e8
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-02 20:42:48 -0800

    Reformat with floating labels

--- frontend/src/pages/SkillsPage/AddSkillForm.jsx
+++ frontend/src/pages/SkillsPage/AddSkillForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
+import Input from '@/components/Inputs/LabeledInput';
 import ColorSelector from './ColorSelector';
 import addSkill from '@/hooks/addSkill';
 import SubmitButton from '@/components/Buttons/SubmitBtn';
@@ -52,7 +53,7 @@ export default function AddSkillForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="container mx-auto p-8 bg-white shadow-xl rounded-2xl">
+    <>
       {showSuccess && (
         <div
           className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-md relative mb-4"
@@ -61,62 +62,72 @@ export default function AddSkillForm() {
           <span className="block sm:inline">Skill added successfully!</span>
         </div>
       )}
-      <h1 className="text-2xl font-semibold text-gray-800 mb-8">Add New Skill</h1>
-      <div className="flex flex-col">
-        <div className="flex flex-col xl:flex-row gap-4 items-baseline">
-          <div>
-            <label className="block text-gray-700 text-xs font-bold mb-2 grow" htmlFor="skill_name">
-              Skill Name
-            </label>
-            <input
-              className="border border-gray-600 rounded-lg shadow-md p-2 w-full h-[40px] bg-white  focus:outline-none focus:ring-2 focus:ring-blue-500"
+      <div className="container mx-auto p-8 bg-white shadow-xl rounded-2xl lg:w-3/4 w-full mb-16">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">
+          Add New Skill
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 md:grid-cols-3 md:gap-4">
+            <Input
+              label="Skill Name"
               type="text"
               id="skill_name"
               name="skill_name"
-              onChange={handleInputChange}
+              placeholder="Skill Name"
               value={formData.skill_name}
-              placeholder="Ex: First Aid"
+              onChange={handleInputChange}
               required
             />
-          </div>
-          <div>
-            <label className="block text-gray-700 text-xs font-bold mb-2" htmlFor="abreviation">
-              Abreviation
-            </label>
-            <input
-              className="border border-gray-600 rounded-lg shadow-md p-2 w-20 h-[40px] bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            <Input
+              label="Abbreviation"
               type="text"
               id="abreviation"
               name="abreviation"
+              placeholder="Abbreviation"
               onChange={handleInputChange}
               value={formData.abreviation}
-              placeholder="FA"
               required
             />
-          </div>
-          <div>
-            <label className="block text-gray-700 text-xs font-bold mb-2" htmlFor="description">
-              Description
-            </label>
-            <input
-              className="border border-gray-600 rounded-lg shadow-md p-2 w-full lg:w-lg h-[40px] bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-              type="text"
-              id="description"
-              name="description"
-              onChange={handleInputChange}
-              value={formData.description}
-              placeholder="Ex: Level 1 First Aid certification completed by Red Cross"
+            {/* <Input
+          label="Description"
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Description"
+          onChange={handleInputChange}
+          value={formData.description}
+          /> */}
+            <ColorSelector
+              onColorChange={(color) =>
+                setFormData({ ...formData, base_color: color })
+              }
             />
           </div>
-          <ColorSelector onColorChange={(color) => setFormData({ ...formData, base_color: color })} />
-        </div>
-        <SubmitButton
-          type="submit"
-          label="Add Skill"
-          disabled={addSkillMutation.isPending}
-          className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
-        />
+          <div className="relative">
+            <textarea
+              id="description"
+              name="description"
+              rows={2}
+              value={formData.description}
+              placeholder="Description"
+              onChange={handleInputChange}
+              className="peer p-2 block w-full placeholder-transparent bg-white rounded-t-lg caret-gray-800 text-gray-800 border-b-2 border-b-gray-600 focus:border-yellow-500 focus:outline-none mt-12"
+            ></textarea>
+            <label
+              htmlFor="comment"
+              className="peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 absolute text-gray-400 left-0 -top-5.5 text-sm transition-all cursor-text"
+            >
+              Description
+            </label>
+          </div>
+          <SubmitButton
+            type="submit"
+            label="Add Skill"
+            disabled={addSkillMutation.isPending}
+            className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
+          />
+        </form>
       </div>
-    </form>
+    </>
   );
 }
--- frontend/src/pages/WorkerListPage/AddWorkerModal/AddWorkerModal.jsx
+++ frontend/src/pages/WorkerListPage/AddWorkerModal/AddWorkerModal.jsx
@@ -4,7 +4,7 @@ import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import CapitalizeFirstLetter from '@/utilities/capitalizeFirstLetter';
 
 import Input from '@/components/Inputs/LabeledInput';
-import SelectInput from '@/components/Inputs/LabeledSelectInput';
+import SelectInput from '@/components/Inputs/FloatingSelectInput';
 import CancelBtn from '@/components/Buttons/CancelBtn';
 import SubmitBtn from '@/components/Buttons/SubmitBtn';
 import Modal from '@/components/Modals/Modal';
@@ -30,7 +30,6 @@ export default function AddWorkerModal({ showModal, onClose, editingWorker }) {
     last_name: '',
     email: '',
     phone_number: '',
-    // current_contract: '',
     agency: '',
   });
 
@@ -66,29 +65,11 @@ export default function AddWorkerModal({ showModal, onClose, editingWorker }) {
     addWorkerMutation.mutate(formData);
   };
 
-  const inputFieldClasses =
-    'mt-1 p-2 block w-full rounded-md border bg-gray-800 border-gray-400 shadow-sm focus:border-blue-500 focus:ring-blue-500';
-  const inputLabelClasses = 'block text-sm font-medium text-gray-200';
-
   return (
-    //     <div
-    //     className={`fixed inset-0 flex items-center justify-center z-50 transition-opacity duration-300 ${
-    //         showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'
-    //     }`}
-    //     >
-    //   <div className="bg-gray-800/75 absolute inset-0" onClick={onClose}></div>
-    //   <dialog
-    //     ref={dialogRef}
-    //     className={` bg-gray-900 text-white p-8 rounded-2xl shadow-xl max-w-lg w-full transform transition-transform duration-300 relative ${
-    //         showModal ? 'scale-100' : 'scale-95'
-    //     }`}
-    //     open={showModal}
-    //     onClose={onClose}
-    //   >
     <Modal show={showModal} onClose={onClose}>
       <div className="flex justify-between items-center pb-3">
-        <h3 className="text-2xl font-semibold text-white">Add New Worker</h3>
-        <button onClick={onClose} className="text-gray-200 hover:text-gray-700">
+        <h3 className="text-2xl font-semibold text-gray-800">Add New Worker</h3>
+        <button onClick={onClose} className="text-gray-400 hover:text-gray-500">
           <svg
             className="h-6 w-6"
             fill="none"
@@ -104,45 +85,47 @@ export default function AddWorkerModal({ showModal, onClose, editingWorker }) {
           </svg>
         </button>
       </div>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <Input
-          type="text"
-          id="first_name"
-          name="first_name"
-          label="First Name"
-          value={CapitalizeFirstLetter(formData.first_name)}
-          onChange={handleInputChange}
-          className={inputFieldClasses}
-          labelClasses={inputLabelClasses}
-        />
-        <Input
-          type="text"
-          id="last_name"
-          name="last_name"
-          label="Last Name"
-          value={CapitalizeFirstLetter(formData.last_name)}
-          onChange={handleInputChange}
-          className={inputFieldClasses}
-          labelClasses={inputLabelClasses}
-        />
-        <Input
-          type="email"
-          id="email"
-          name="email"
-          label="Email"
-          value={formData.email}
-          onChange={handleInputChange}
-          className={inputFieldClasses}
-        />
-        <Input
-          type="text"
-          id="phone_number"
-          name="phone_number"
-          label="Phone Number"
-          value={formData.phone_number}
-          onChange={handleInputChange}
-          className={inputFieldClasses}
-        />
+      <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 md:items-end">
+          <Input
+            type="text"
+            id="first_name"
+            name="first_name"
+            label="First Name"
+            placeholder="First Name"
+            value={CapitalizeFirstLetter(formData.first_name)}
+            onChange={handleInputChange}
+          />
+          <Input
+            type="text"
+            id="last_name"
+            name="last_name"
+            label="Last Name"
+            placeholder="Last Name"
+            value={CapitalizeFirstLetter(formData.last_name)}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 md:items-end">
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            label="Email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <Input
+            type="text"
+            id="phone_number"
+            name="phone_number"
+            label="Phone Number"
+            placeholder="Phone Number"
+            value={formData.phone_number}
+            onChange={handleInputChange}
+          />
+        </div>
         {agencies && (
           <SelectInput
             label="Agency"
@@ -153,7 +136,6 @@ export default function AddWorkerModal({ showModal, onClose, editingWorker }) {
             onChange={handleInputChange}
             required
             options={agencyNames}
-            className={inputFieldClasses}
           ></SelectInput>
         )}
         <div className="mt-6 flex justify-end gap-3">
@@ -176,9 +158,4 @@ export default function AddWorkerModal({ showModal, onClose, editingWorker }) {
       </form>
     </Modal>
   );
-}
-
-{
-  /* </dialog>
-</div> */
 }
