bf4c19edf8e93de4350687308e15034b6388360f
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-06 07:23:00 -0800

    Increase top margin on section

--- frontend/src/pages/WorkerListPage/EditWorkerModal/WorkerDetails.jsx
+++ frontend/src/pages/WorkerListPage/EditWorkerModal/WorkerDetails.jsx
@@ -12,7 +12,7 @@ export default function WorkerDetails({
     <Section>
       <SectionHeader title="Work Details" />
       <div>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-x-4 mt-8">
           <SelectInput
             label="Agency"
             type="text"
