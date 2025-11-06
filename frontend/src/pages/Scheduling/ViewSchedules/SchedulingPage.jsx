56f4cae4bb3b40ceb904515e6dea5731e37c418d
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-06 07:30:45 -0800

    Create new parent component for rendering schedule list or schedule update view

---
+++ frontend/src/pages/Scheduling/ViewSchedules/SchedulingPage.jsx
@@ -0,0 +1,15 @@
+import { Outlet, useParams } from 'react-router';
+
+import ScheduleListComponent from '@/pages/Scheduling/ViewSchedules/ScheduleListComponent';
+
+export default function SchedulingPage() {
+    const { scheduleId } = useParams();
+    console.log(scheduleId);
+
+
+  return (
+    <div className="container mx-auto p-8 bg-white shadow-md rounded-2xl">
+      {scheduleId ? <Outlet /> : <ScheduleListComponent />}
+    </div>
+  );
+}
