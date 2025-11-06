2353e3937a77b94737d37f1118133871cebcde86
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-06 07:21:51 -0800

    Add react-router logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
 import { useEffect, useState } from 'react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Outlet } from 'react-router';
 
 import TopNavBar from './components/NavBars/TopNavBar';
 import SideNavBar from './components/NavBars/SideNavBar';
-import Dashboard from './pages/Dashboard/DashboardPage';
-import Workers from './pages/WorkerListPage/WorkersPage';
-import Skills from './pages/SkillsPage/SkillsPage';
-import Schedule from './pages/Schedule/SchedulePage';
-import Messaging from './pages/Messaging/MessagingPage';
-import Modal from './components/Modals/Modal';
-import LogoutModal from './components/Modals/Logout';
 
 import Login from './components/Authentication/LoginPage';
-import handleLogout from './components/Authentication/LogoutFunction';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -34,28 +26,12 @@ function App() {
     setIsActive(name);
   };
 
-  const mainContent = (isActive) => {
-    if (isActive === 'Dashboard') {
-      return <Dashboard />;
-    } else if (isActive === 'Workers') {
-      return <Workers />;
-    } else if (isActive === 'Skills') {
-      return <Skills />;
-    } else if (isActive === 'Scheduling') {
-      return <Schedule />;
-    } else if (isActive === 'Messaging') {
-      return <Messaging />;
-    } else if (isActive === 'Settings') {
-      return <Settings />;
-    }
-  };
-
   if (isLoggedIn === false) {
     return <Login />;
   } else {
     return (
       <QueryClientProvider client={queryClient}>
-        <div className="flex w-full min-h-screen bg-gray-100/50">
+        <div className="flex w-full min-h-screen bg-gray-100/75">
           <SideNavBar
             activeLink={isActive}
             onLinkClick={handleSideLinkClick}
@@ -67,10 +43,11 @@ function App() {
           />
           <div className="flex flex-col flex-1 h-screen overflow-y-auto">
             <TopNavBar />
-            <main className="m-4 flex-1">{mainContent(isActive)}</main>
+            <main className="m-4 flex-1">
+              <Outlet />
+            </main>
           </div>
         </div>
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     );
   }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,39 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router';
+
+import Dashboard from '@/pages/Dashboard/DashboardPage';
+import Workers from '@/pages/WorkerListPage/WorkersPage';
+import Skills from '@/pages/SkillsPage/SkillsPage';
+import Scheduling from '@/pages/Scheduling/SchedulingPage';
+import Schedule from '@/pages/Scheduling/EditScheduleModal/Schedule';
+import Messaging from '@/pages/Messaging/MessagingPage';
+import LoginPage from '@/components/Authentication/LoginPage';
+
 import './index.css';
 import App from './App.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const client = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="workers" element={<Workers />} />
+            <Route path="skills" element={<Skills />} />
+            <Route path="scheduling" element={<Scheduling />}>
+              <Route path=":scheduleId" element={<Schedule />} />
+            </Route>
+            <Route path="messaging" element={<Messaging />} />
+            {/* <Route path="settings" element={<Settings />} /> */}
+          </Route>
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
 );
