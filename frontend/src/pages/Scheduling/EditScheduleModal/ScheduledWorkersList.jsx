b2854bce19e51032fdd3d451ab73019edf5ccb50
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-10 22:33:49 -0800

    Whitespace

--- frontend/src/pages/Scheduling/EditScheduleModal/ScheduledWorkersList.jsx
+++ frontend/src/pages/Scheduling/EditScheduleModal/ScheduledWorkersList.jsx
@@ -164,144 +164,144 @@ export default function ScheduledWorkerList({ shiftId, scheduledWorkersId }) {
 
       <div className="mt-4">
         <div className="flex justify-between align-center items-center mb-2">
-            <Search onSearch={handleSearch} searchTerm={searchTerm} />
-            <button
-              className="bg-green-600 px-4 py-2 rounded-lg hover:bg-green-700 transition-colors duration-200 text-white font-medium cursor-pointer"
-              onClick={handleOpenSendSMSModal}
-            >
-              {isSendingSMS ? 'Sending...' : 'Notify Workers'}
-            </button>
-          </div>
-          <div className="overflow-x-auto border border-gray-300 rounded-lg mb-8">
-            <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
-              <thead className="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
-                <tr>
-                  <th className="py-1 px-6 text-left border-r border-gray-300">
-                    <input
-                      type="checkbox"
+          <Search onSearch={handleSearch} searchTerm={searchTerm} />
+          <button
+            className="bg-green-600 px-4 py-2 rounded-lg hover:bg-green-700 transition-colors duration-200 text-white font-medium cursor-pointer"
+            onClick={handleOpenSendSMSModal}
+          >
+            {isSendingSMS ? 'Sending...' : 'Notify Workers'}
+          </button>
+        </div>
+        <div className="overflow-x-auto border border-gray-300 rounded-lg mb-8">
+          <table className="w-full table-auto border-collapse border border-gray-300 rounded-lg overflow-hidden">
+            <thead className="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
+              <tr>
+                <th className="py-1 px-6 text-left border-r border-gray-300">
+                  <input
+                    type="checkbox"
                     id="select_all_workers"
                     name="select_all_workers"
-                      value="yes"
+                    value="yes"
                     onChange={handleSelectAllWorkers}
-                    />
-                  </th>
-                  <th className="py-1 px-6 text-left border-r border-gray-300">
-                    Name
-                  </th>
-                  <th className="py-1 px-6 text-left border-r border-gray-300">
-                    Position
-                  </th>
-                  <th className="py-1 px-6 text-left border-r border-gray-300">
-                    Agency
-                  </th>
-                  <th className="py-1 px-6 text-left border-r border-gray-300">
-                    <div className="flex justify-between items-center">
-                      Rating{' '}
-                      <button
-                        onClick={handleRatingSort}
-                        className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
-                      >
+                  />
+                </th>
+                <th className="py-1 px-6 text-left border-r border-gray-300">
+                  Name
+                </th>
+                <th className="py-1 px-6 text-left border-r border-gray-300">
+                  Position
+                </th>
+                <th className="py-1 px-6 text-left border-r border-gray-300">
+                  Agency
+                </th>
+                <th className="py-1 px-6 text-left border-r border-gray-300">
+                  <div className="flex justify-between items-center">
+                    Rating{' '}
+                    <button
+                      onClick={handleRatingSort}
+                      className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
+                    >
                       <SortIcon />
-                      </button>
-                    </div>
-                  </th>
-                  <th className="py-3 px-6 text-left">Skills</th>
-                </tr>
-              </thead>
-              <tbody className="text-gray-600 text-sm font-light">
+                    </button>
+                  </div>
+                </th>
+                <th className="py-3 px-6 text-left">Skills</th>
+              </tr>
+            </thead>
+            <tbody className="text-gray-600 text-sm font-light">
               {workers && workers.length > 0 ? (
                 workers.map((worker) => (
-                    <tr
-                      key={worker.id}
-                      className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-200"
+                  <tr
+                    key={worker.id}
+                    className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-200"
+                  >
+                    <td className="py-3 px-6 text-left whitespace-nowrap border-r border-gray-200">
+                      <input
+                        type="checkbox"
+                        id={`contract_workers_${worker.id}`}
+                        name="contract_workers"
+                        value={worker.id}
+                        checked={selectedWorkerIds.includes(worker.id)}
+                        onChange={(e) =>
+                          handleWorkerCheck(
+                            shiftIndex,
+                            e.target.value,
+                            e.target.checked
+                          )
+                        }
+                      />
+                    </td>
+                    <td className="py-3 px-6 text-left border-r border-gray-200">
+                      {worker.first_name} {worker.last_name}
+                    </td>
+                    <td className="py-3 px-6 text-left border-r border-gray-200">
+                      {worker.position}
+                    </td>
+                    <td className="py-3 px-6 text-left border-r border-gray-200">
+                      {worker.agency_details}
+                    </td>
+                    <td
+                      className="py-3 px-6 text-left border-r border-gray-200 relative"
+                      onMouseEnter={() => setHoveredWorkerId(worker.id)}
+                      onMouseLeave={() => setHoveredWorkerId(null)}
                     >
-                      <td className="py-3 px-6 text-left whitespace-nowrap border-r border-gray-200">
-                        <input
-                          type="checkbox"
-                          id={`contract_workers_${worker.id}`}
-                          name="contract_workers"
-                          value={worker.id}
-                          checked={selectedWorkerIds.includes(worker.id)}
-                          onChange={(e) =>
-                            handleWorkerCheck(
-                              shiftIndex,
-                              e.target.value,
-                              e.target.checked
-                            )
-                          }
-                        />
-                      </td>
-                      <td className="py-3 px-6 text-left border-r border-gray-200">
-                        {worker.first_name} {worker.last_name}
-                      </td>
-                      <td className="py-3 px-6 text-left border-r border-gray-200">
-                        {worker.position}
-                      </td>
-                      <td className="py-3 px-6 text-left border-r border-gray-200">
-                        {worker.agency_details}
-                      </td>
-                      <td
-                        className="py-3 px-6 text-left border-r border-gray-200 relative"
-                        onMouseEnter={() => setHoveredWorkerId(worker.id)}
-                        onMouseLeave={() => setHoveredWorkerId(null)}
-                      >
-                        {hoveredWorkerId === worker.id && (
-                          <div className="absolute bottom-1/2 left-1/2 -translate-x-1/2 mb-2 bg-white border border-gray-300 p-2 rounded shadow-lg text-sm z-10 whitespace-nowrap">
+                      {hoveredWorkerId === worker.id && (
+                        <div className="absolute bottom-1/2 left-1/2 -translate-x-1/2 mb-2 bg-white border border-gray-300 p-2 rounded shadow-lg text-sm z-10 whitespace-nowrap">
                           {worker.rating ? worker.rating.average_rating : 'N/A'}{' '}
-                            out of 5
-                          </div>
-                        )}
-                        <Rating
-                          initialValue={
+                          out of 5
+                        </div>
+                      )}
+                      <Rating
+                        initialValue={
                           worker.rating !== null && worker.rating.average_rating
                             ? worker.rating.average_rating
-                              : 0
-                          }
-                          {...starRating}
-                        />
-                      </td>
-                      <td className="text-white py-3 px-6 text-left border-r border-gray-200">
-                        <div className="flex flex-wrap gap-2">
-                          {worker.worker_skills !== null &&
-                            worker.worker_skills?.map((skill, index) => (
-                              <span
-                                className={`text-white text-xs font-semibold px-2.5 py-1 rounded-full shadow-md transition-colors duration-200 tooltip ${
-                                  skillColorClasses[skill.skill.base_color]?.[
-                                    skill.level
-                                  ] || 'bg-gray-400 hover:bg-gray-500'
-                                }`}
-                                key={index}
-                              >
-                                {skill.skill.abreviation}LV{skill.level}
-                                <div className="tooltip-content cursor-pointer">
-                                  {skill.skill.skill_name} level {skill.level}
-                                </div>
-                              </span>
-                            ))}
-                        </div>
-                      </td>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="7" className="text-center">
-                      <div className="flex justify-center items-center h-[100px]">
-                        <div className="flex justify-center items-center h-auto w-fit mx-auto bg-yellow-100 text-yellow-700 p-4 rounded-lg">
-                          No workers found, please add workers to schedule.
-                        </div>
+                            : 0
+                        }
+                        {...starRating}
+                      />
+                    </td>
+                    <td className="text-white py-3 px-6 text-left border-r border-gray-200">
+                      <div className="flex flex-wrap gap-2">
+                        {worker.worker_skills !== null &&
+                          worker.worker_skills?.map((skill, index) => (
+                            <span
+                              className={`text-white text-xs font-semibold px-2.5 py-1 rounded-full shadow-md transition-colors duration-200 tooltip ${
+                                skillColorClasses[skill.skill.base_color]?.[
+                                  skill.level
+                                ] || 'bg-gray-400 hover:bg-gray-500'
+                              }`}
+                              key={index}
+                            >
+                              {skill.skill.abreviation}LV{skill.level}
+                              <div className="tooltip-content cursor-pointer">
+                                {skill.skill.skill_name} level {skill.level}
+                              </div>
+                            </span>
+                          ))}
                       </div>
                     </td>
                   </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
-      <SMSNotificationModal
-        show={openSMSModal}
-        onClose={handleOpenSendSMSModal}
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="7" className="text-center">
+                    <div className="flex justify-center items-center h-[100px]">
+                      <div className="flex justify-center items-center h-auto w-fit mx-auto bg-yellow-100 text-yellow-700 p-4 rounded-lg">
+                        No workers found, please add workers to schedule.
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+        <SMSNotificationModal
+          show={openSMSModal}
+          onClose={handleOpenSendSMSModal}
           workerCount={workers.length}
-        sendSMS={sendSMS}
-      />
+          sendSMS={sendSMS}
+        />
         <Pagination
           page={page}
           setPage={setPage}
