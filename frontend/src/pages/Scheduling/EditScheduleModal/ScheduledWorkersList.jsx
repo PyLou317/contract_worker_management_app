3feac6447b11eece333f1bf2d11c85571c761892
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-10 22:29:26 -0800

    Move sort Icon to separate file

--- frontend/src/pages/Scheduling/EditScheduleModal/ScheduledWorkersList.jsx
+++ frontend/src/pages/Scheduling/EditScheduleModal/ScheduledWorkersList.jsx
@@ -200,20 +200,7 @@ export default function ScheduledWorkerList({ shiftId, scheduledWorkersId }) {
                         onClick={handleRatingSort}
                         className="text-gray-500 hover:text-gray-700 transition-colors duration-200"
                       >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="size-6 cursor-pointer"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                          />
-                        </svg>
+                      <SortIcon />
                       </button>
                     </div>
                   </th>
