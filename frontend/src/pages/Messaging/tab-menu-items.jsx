7ba05cda4ba8e69215d11bc67ab792c5dfa4ca62
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:16:23 -0800

    New file: tab menu items for messaging

---
+++ frontend/src/pages/Messaging/tab-menu-items.jsx
@@ -0,0 +1,16 @@
+import WorkersIcon from '@/Icons/WorkersIcon';
+
+const tabItems = [
+  {
+    name: 'Request Workers',
+    path: '/messaging/request-workers',
+    icon: WorkersIcon,
+  },
+  {
+    name: 'Archived Messages',
+    path: '/messaging/view-messages',
+    icon: WorkersIcon,
+  },
+];
+
+export default tabItems;
