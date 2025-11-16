a4e06a5253efacfacc180b00594c1edd4116b342
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:20:44 -0800

    Style: add bottom border line full width

--- frontend/src/pages/WorkerListPage/EditWorkerModal/SectionHeader.jsx
+++ frontend/src/pages/WorkerListPage/EditWorkerModal/SectionHeader.jsx
@@ -1,10 +1,13 @@
 import { Children } from 'react';
 
-export default function SectionHeader({ title, children }) {
+export default function SectionHeader({ title, children, addClasses }) {
   return (
-    <div className="mb-4">
-      <h3 className="text-2xl font-semibold text-gray-800">{title}</h3>
-      {children}
-    </div>
+    <>
+      <div className={`mb-4 ${addClasses}`}>
+        <h3 className="text-2xl font-semibold">{title}</h3>
+        {children}
+      </div>
+      <div className="border-b border-gray-200 mt-6 mb-10"></div>
+    </>
   );
 }
