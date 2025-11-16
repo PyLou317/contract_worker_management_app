bdf9256dc0bcf3d16049e0725f14836a96639c8c
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:17:11 -0800

    Feature: Add toggle add skill form

--- frontend/src/pages/SkillsPage/AddSkillForm.jsx
+++ frontend/src/pages/SkillsPage/AddSkillForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import Input from '@/components/Inputs/LabeledInput';
 import ColorSelector from './ColorSelector';
@@ -9,6 +9,7 @@ import getAcronym from '@/utilities/getAcronym';
 import SectionHeader from '../WorkerListPage/EditWorkerModal/SectionHeader';
 
 export default function AddSkillForm() {
+  const [toggleAddSkill, setToggleAddSkill] = useState(false);
   const [formData, setFormData] = useState({
     skill_name: '',
     abreviation: '',
@@ -53,6 +54,10 @@ export default function AddSkillForm() {
     addSkillMutation.mutate(formData);
   };
 
+  const handleToggleAddSkill = () => {
+    setToggleAddSkill(!toggleAddSkill);
+  };
+
   return (
     <>
       {showSuccess && (
@@ -63,60 +68,102 @@ export default function AddSkillForm() {
           <span className="block sm:inline">Skill added successfully!</span>
         </div>
       )}
-      <div className="container mx-auto p-8 bg-white shadow-xl rounded-2xl lg:w-3/4 w-full mb-16">
-        <SectionHeader title="Add New Skill" />
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-1 md:grid-cols-3 md:gap-4">
-            <Input
-              label="Skill Name"
-              type="text"
-              id="skill_name"
-              name="skill_name"
-              placeholder="Skill Name"
-              value={formData.skill_name}
-              onChange={handleInputChange}
-              required
-            />
-            <Input
-              label="Abbreviation"
-              type="text"
-              id="abreviation"
-              name="abreviation"
-              placeholder="Abbreviation"
-              onChange={handleInputChange}
-              value={formData.abreviation}
-              required
-            />
-            <ColorSelector
-              onColorChange={(color) =>
-                setFormData({ ...formData, base_color: color })
-              }
-            />
-          </div>
-          <div className="relative">
-            <textarea
-              id="description"
-              name="description"
-              rows={2}
-              value={formData.description}
-              placeholder="Description"
-              onChange={handleInputChange}
-              className="peer p-2 block w-full placeholder-transparent bg-white rounded-t-lg caret-gray-800 text-gray-800 border-b-2 border-b-gray-600 focus:border-yellow-500 focus:outline-none mt-12"
-            ></textarea>
-            <label
-              htmlFor="comment"
-              className="peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 absolute text-gray-400 left-0 -top-5.5 text-sm transition-all cursor-text"
-            >
-              Description
-            </label>
-          </div>
-          <SubmitButton
-            type="submit"
-            label="Add Skill"
-            disabled={addSkillMutation.isPending}
-            className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
+      <div className="container mx-auto p-8 bg-white shadow-xl rounded-2xl lg:w-3/4 w-full mb-16 hover:scale-101">
+        <div
+          className="flex justify-between cursor-pointer hover:text-yellow-400"
+          onClick={handleToggleAddSkill}
+        >
+          <SectionHeader
+            title="Add New Skill"
+            addClasses="hover:text-yellow-400"
           />
-        </form>
+          {toggleAddSkill ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          )}
+        </div>
+
+        {toggleAddSkill && (
+          <form onSubmit={handleSubmit} className="mt-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 md:gap-4">
+              <Input
+                label="Skill Name"
+                type="text"
+                id="skill_name"
+                name="skill_name"
+                placeholder="Skill Name"
+                value={formData.skill_name}
+                onChange={handleInputChange}
+                required
+              />
+              <Input
+                label="Abbreviation"
+                type="text"
+                id="abreviation"
+                name="abreviation"
+                placeholder="Abbreviation"
+                onChange={handleInputChange}
+                value={formData.abreviation}
+                required
+              />
+              <ColorSelector
+                onColorChange={(color) =>
+                  setFormData({ ...formData, base_color: color })
+                }
+              />
+            </div>
+            <div className="relative">
+              <textarea
+                id="description"
+                name="description"
+                rows={2}
+                value={formData.description}
+                placeholder="Description"
+                onChange={handleInputChange}
+                className="peer p-2 block w-full placeholder-transparent bg-white rounded-t-lg caret-gray-800 text-gray-800 border-b-2 border-b-gray-600 focus:border-yellow-500 focus:outline-none mt-12"
+              ></textarea>
+              <label
+                htmlFor="comment"
+                className="peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 absolute text-gray-400 left-0 -top-5.5 text-sm transition-all cursor-text"
+              >
+                Description
+              </label>
+            </div>
+            <SubmitButton
+              type="submit"
+              label="Add Skill"
+              disabled={addSkillMutation.isPending}
+              className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
+            />
+          </form>
+        )}
       </div>
     </>
   );
