ab502876766deace3f9fcead895078468afa2392
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:21:53 -0800

    Feature: add agency settings page

---
+++ frontend/src/pages/Settings/Agencies/AgenciesPage.jsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/PageContainer';
+import SectionHeader from '@/pages/WorkerListPage/EditWorkerModal/SectionHeader';
+
+export default function AgenciesPage() {
+  return (
+    <PageContainer>
+      <SectionHeader title="Agencies Settings" />
+    </PageContainer>
+  );
+}
