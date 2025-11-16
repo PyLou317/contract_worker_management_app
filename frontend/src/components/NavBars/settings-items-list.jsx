8a63ecd46ea812651728660a6f6b4dda993900e5
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:10:20 -0800

    add link paths

--- frontend/src/components/NavBars/settings-items-list.jsx
+++ frontend/src/components/NavBars/settings-items-list.jsx
@@ -1,10 +1,12 @@
 const settingsItem = [
   {
     name: 'Settings',
+    path: '/settings/account',
     icon: <i className="fa-solid fa-gear"></i>,
   },
   {
     name: 'Logout',
+    path: '/logout',
     icon: <i className="fa-solid fa-right-from-bracket"></i>,
   },
 ];
