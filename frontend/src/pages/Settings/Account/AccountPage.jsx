4352a82aa58849dca1b91a6922665dd66450960d
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:22:13 -0800

    Feature: add acount settings page

---
+++ frontend/src/pages/Settings/Account/AccountPage.jsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/PageContainer';
+import SectionHeader from '@/pages/WorkerListPage/EditWorkerModal/SectionHeader';
+
+export default function AccountPage() {
+  return (
+    <PageContainer>
+      <SectionHeader title="Account Settings" />
+    </PageContainer>
+  );
+}
