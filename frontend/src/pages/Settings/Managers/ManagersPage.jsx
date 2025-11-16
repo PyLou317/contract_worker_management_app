bfffac4bff59d3348dfd2ba38cad7db2051b53f8
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-15 23:16:47 -0800

    Feature: Manager setting page

---
+++ frontend/src/pages/Settings/Managers/ManagersPage.jsx
@@ -0,0 +1,10 @@
+import PageContainer from '@/components/PageContainer';
+import SectionHeader from '@/pages/WorkerListPage/EditWorkerModal/SectionHeader';
+
+export default function ManagersPage() {
+  return (
+    <PageContainer>
+      <SectionHeader title="Manager Settings" />
+    </PageContainer>
+  );
+}
