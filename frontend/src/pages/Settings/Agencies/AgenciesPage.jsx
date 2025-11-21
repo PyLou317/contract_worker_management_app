0d417457b65bb4f2db2ccf5616ace59fbe4933c3
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-20 19:09:34 -0700

    feat: add agency form and agency list

--- frontend/src/pages/Settings/Agencies/AgenciesPage.jsx
+++ frontend/src/pages/Settings/Agencies/AgenciesPage.jsx
@@ -1,10 +1,15 @@
 import PageContainer from '@/components/PageContainer';
 import SectionHeader from '@/pages/WorkerListPage/EditWorkerModal/SectionHeader';
+import AddAgencyForm from '@/pages/Settings/Agencies/AddAgencyForm';
+import AgenciesList from '@/pages/settings/Agencies/AgenciesList';
 
 export default function AgenciesPage() {
   return (
     <PageContainer>
       <SectionHeader title="Agencies Settings" />
+      <AddAgencyForm />
+      <div className="mt-8 w-full border-t border-gray-200"></div>
+      <AgenciesList />
     </PageContainer>
   );
 }
