3df2c17079192d207eccd4551ec80ebadcf8ed68
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-20 19:05:46 -0700

    feat: add user data view and url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
-from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import *
 
 urlpatterns = [
-
+    path('user/', CurrentUserView.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
