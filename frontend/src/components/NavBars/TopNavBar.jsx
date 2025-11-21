5b4afe2e07ad723e034c04a32b5aa34ce431598b
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-20 19:03:19 -0700

    feat: add searchbar and alerts icon

--- frontend/src/components/NavBars/TopNavBar.jsx
+++ frontend/src/components/NavBars/TopNavBar.jsx
@@ -43,7 +43,24 @@ export default function TopNavBar() {
 
   return (
     <nav className="p-6 border-b-2 border-gray-200 w-full bg-white flex justify-between items-center space-x-4">
-      <h1 className="text-2xl font-semibold">Welcome, Lucas!</h1>
+      <h1 className="text-2xl font-semibold">{content}</h1>
+      <div className="flex items-center gap-3">
+        <SearchBar />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-8 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+          />
+        </svg>
+      </div>
     </nav>
   );
 }
