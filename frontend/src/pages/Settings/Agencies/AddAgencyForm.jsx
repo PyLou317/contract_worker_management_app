4fd5aea693ef564a9388103d898abf414f2c8a97
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-20 19:07:39 -0700

    feat: add form for adding a new agency in settings

---
+++ frontend/src/pages/Settings/Agencies/AddAgencyForm.jsx
@@ -0,0 +1,269 @@
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import sendData from '@/hooks/sendData';
+
+import Input from '@/components/Inputs/LabeledInput';
+import SubmitButton from '@/components/Buttons/SubmitBtn';
+import SectionHeader from '@/pages/WorkerListPage/EditWorkerModal/SectionHeader';
+
+export default function AddSkillForm() {
+  const [toggleAddAgency, setToggleAddAgency] = useState(false);
+  const [formData, setFormData] = useState({
+    address: '',
+    city: '',
+    contact_first_name: '',
+    contact_last_name: '',
+    contact_email: '',
+    name: '',
+    phone_number: '',
+    postal_code: '',
+    province: '',
+    website: '',
+  });
+
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'website') {
+      let finalValue = value;
+
+      const protocolRegex = /^(http|https):\/\//i;
+
+      if (value && !protocolRegex.test(value)) {
+        finalValue = 'https://' + value;
+      }
+
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: finalValue,
+      }));
+    } else {
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
+
+  const queryClient = useQueryClient();
+  const addAgencyMutation = useMutation({
+    mutationFn: (payload) =>
+      sendData(payload.formData, payload.endpoint, payload.method),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['agencies'] });
+      setFormData({
+        address: '',
+        city: '',
+        contact_first_name: '',
+        contact_last_name: '',
+        contact_email: '',
+        name: '',
+        phone_number: '',
+        postal_code: '',
+        province: '',
+        website: '',
+      });
+      setShowSuccess(true);
+      setTimeout(() => setShowSuccess(false), 3000);
+    },
+    onError: (error) => {
+      console.error('Error adding agency:', error);
+      alert('Failed to add agency. Please try again. ' + error.message);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      formData: formData,
+      endpoint: `/agencies/`,
+      method: 'POST',
+    };
+    addAgencyMutation.mutate(payload);
+  };
+
+  const handleToggleAddAgency = () => {
+    setToggleAddAgency(!toggleAddAgency);
+  };
+
+  return (
+    <>
+      {showSuccess && (
+        <div
+          className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-md relative mb-4"
+          role="alert"
+        >
+          <span className="block sm:inline">Agency added successfully!</span>
+        </div>
+      )}
+      <div className="container mx-auto p-8 bg-white shadow-xl rounded-2xl lg:w-3/4 w-full mb-16 hover:scale-101">
+        <div
+          className={`flex justify-between items-center cursor-pointer ${toggleAddAgency ? 'mb-4' : null}`}
+          onClick={handleToggleAddAgency}
+        >
+          <SectionHeader title="Add New Agency" />
+          {toggleAddAgency ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          )}
+        </div>
+
+        {toggleAddAgency && (
+          <form onSubmit={handleSubmit} className="mt-4">
+            <div className="grid grid-cols-1 md:gap-4">
+              <Input
+                label="Agency Name"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Agency Name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+
+            <h1 className="font-semibold text-md mt-12">Contact:</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-10 mt-8">
+              <Input
+                label="Contact First Name"
+                type="text"
+                id="contact_first_name"
+                name="contact_first_name"
+                placeholder="Contact First Name"
+                onChange={handleInputChange}
+                value={formData.contact_first_name}
+                required
+              />
+              <Input
+                label="Contact Last Name"
+                type="text"
+                id="contact_last_name"
+                name="contact_last_name"
+                placeholder="Contact Last Name"
+                onChange={handleInputChange}
+                value={formData.contact_last_name}
+                required
+              />
+              <Input
+                label="Contact Number"
+                type="text"
+                id="phone_number"
+                name="phone_number"
+                placeholder="Contact Number"
+                onChange={handleInputChange}
+                value={formData.phone_number}
+                required
+              />
+              <Input
+                label="Contact Email"
+                type="text"
+                id="contact_email"
+                name="contact_email"
+                placeholder="Contact Email"
+                onChange={handleInputChange}
+                value={formData.contact_email}
+                required
+              />
+            </div>
+
+            <h1 className="font-semibold text-md mt-12">Address:</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-x-4 mt-10">
+              <Input
+                label="Street"
+                type="text"
+                id="address"
+                name="address"
+                placeholder="Street"
+                onChange={handleInputChange}
+                value={formData.address}
+                required
+              />
+              <Input
+                label="City"
+                type="text"
+                id="city"
+                name="city"
+                placeholder="City"
+                onChange={handleInputChange}
+                value={formData.city}
+                required
+              />
+              <Input
+                label="Province"
+                type="text"
+                id="province"
+                name="province"
+                placeholder="Province"
+                onChange={handleInputChange}
+                value={formData.province}
+                required
+              />
+              <Input
+                label="Postal Code"
+                type="text"
+                id="postal_code"
+                name="postal_code"
+                placeholder="Postal Code"
+                onChange={handleInputChange}
+                value={formData.postal_code}
+                required
+              />
+            </div>
+
+            <h1 className="font-semibold text-md mt-12">Website:</h1>
+            <div className="grid grid-cols-1 md:gap-y-10 md:gap-x-4 mt-10">
+              <Input
+                label="Website"
+                type="text"
+                id="website"
+                name="website"
+                placeholder="Website"
+                onChange={handleInputChange}
+                value={formData.website}
+                required
+              />
+            </div>
+            <SubmitButton
+              type="submit"
+              label="Add Agency"
+              disabled={addAgencyMutation.isPending}
+              className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
+            />
+          </form>
+        )}
+      </div>
+    </>
+  );
+}
