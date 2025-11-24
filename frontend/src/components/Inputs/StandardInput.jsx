2ef063e7c13c5cfa6ab0cbf4f65a458cd8043870
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-23 19:38:58 -0800

    style: remove text and caret color from classes

--- frontend/src/components/Inputs/StandardInput.jsx
+++ frontend/src/components/Inputs/StandardInput.jsx
@@ -3,7 +3,7 @@ export default function Input(props) {
     <input
       {...props}
       className={`
-        peer p-2 block w-full placeholder-transparent caret-gray-800 text-gray-800 border-b-2 border-b-gray-500 focus:border-yellow-500 focus:outline-none
+        peer p-2 block w-full placeholder-transparent border-b-2 border-b-gray-500 focus:border-yellow-500 focus:outline-none
         ${props.className || ''}
       `}
       placeholder={props.placeholder}
