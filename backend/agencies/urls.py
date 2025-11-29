449040694af78aa8ca03ebd4c66bb5babf221306
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-29 15:43:05 -0800

    feat: add agency detail api endpoint

--- backend/agencies/urls.py
+++ backend/agencies/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('agencies/', AgencyListViewAPI.as_view()),
+    path('agencies/<int:pk>/', AgencyDetailUpdateViewAPI.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
