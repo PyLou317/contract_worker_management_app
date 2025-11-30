0679983a2773f99ca95729097c07e125a79551b0
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-11-29 16:02:29 -0800

    feat: add userDetailForm

--- frontend/src/pages/Settings/Account/AccountPage.jsx
+++ frontend/src/pages/Settings/Account/AccountPage.jsx
@@ -1,10 +1,12 @@
 import PageContainer from '@/components/PageContainer';
 import SectionHeader from '@/pages/WorkerListPage/EditWorkerModal/SectionHeader';
+import UserDetailForm from './UserDetailsForm';
 
 export default function AccountPage() {
   return (
     <PageContainer>
       <SectionHeader title="Account Settings" />
+      <UserDetailForm />
     </PageContainer>
   );
 }
