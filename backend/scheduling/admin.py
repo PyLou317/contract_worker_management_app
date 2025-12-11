8aede34392d85d61f7f2bcfee03b16ccba6dbaf1
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-10 21:52:31 -0800

    add area field to manager admin

--- backend/scheduling/admin.py
+++ backend/scheduling/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from scheduling.models import Schedule, Shift, Area, Manager
 
-
-# Register your models here.
 admin.site.register(Schedule)
 admin.site.register(Shift)
 admin.site.register(Area)
 
 class ManagerAdmin(admin.ModelAdmin):
-    fields = ('id', 'name', 'organization', 'email') 
+    fields = ('id', 'name', 'area','organization', 'email') 
     readonly_fields = ('id',) 
     
-admin.site.register(Manager, ManagerAdmin)
+admin.site.register(Manager)
