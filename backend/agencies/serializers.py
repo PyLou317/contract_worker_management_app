71d22638da2d78e3386587e17369bba308435dde
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-10 21:49:58 -0800

    feature: add create method

--- backend/agencies/serializers.py
+++ backend/agencies/serializers.py
@@ -6,3 +6,19 @@ class StaffingAgencySerializer(serializers.ModelSerializer):
     class Meta:
         model = StaffingAgency
         fields = '__all__'
+        read_only_fields = ('organization',)
+        
+    def create(self, validated_data):
+        request = self.context.get('request', None)
+        
+        if not request:
+            raise serializers.ValidationError({'detail': 'Request context missing for organization assignment.'})
+        
+        organization = request.user.organization
+        
+        if not organization:
+            raise serializers.ValidationError({'detail': 'You must be associated with an organization to create an agency.'})
+        
+        validated_data['organization'] = organization
+        agency = super().create(validated_data)
+        return agency
