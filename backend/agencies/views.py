fee19bd92ee0806ef2fc50f053cd52381b0c3088
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-10 21:50:30 -0800

    adjust return query to only return data that user is apart of

--- backend/agencies/views.py
+++ backend/agencies/views.py
@@ -9,6 +9,18 @@ class AgencyListViewAPI(generics.ListCreateAPIView):
     queryset = StaffingAgency.objects.all()
     serializer_class = StaffingAgencySerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return StaffingAgency.objects.none()
+        
+        user_organization = user.organization
+        if not user_organization:
+            return StaffingAgency.objects.none()
+        
+        queryset = super().get_queryset().filter(organization=user_organization)
+        return queryset
 
 
 class AgencyDetailUpdateViewAPI(generics.RetrieveUpdateDestroyAPIView):
