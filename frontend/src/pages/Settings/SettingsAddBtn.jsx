ea85f4e451fb1ca97b42e6f0495c1e94914adf6b
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-14 21:02:17 -0800

    add logic for which context to load

--- frontend/src/pages/Settings/SettingsAddBtn.jsx
+++ frontend/src/pages/Settings/SettingsAddBtn.jsx
@@ -1,19 +1,38 @@
 import { useContext } from 'react';
 import { AgencyPageContext } from './Agencies/agency-page-context';
+import { ManagerPageContext } from './Managers/manager-page-context';
 
 export default function SettingsAddBtn({ label, extraClasses, ...props }) {
-  const { toggleAddAgency, handleToggleAddAgency } =
-    useContext(AgencyPageContext);
+  function getSecondWord(str) {
+    const words = str.split(' ');
+    return words[1];
+  }
+  const strippedLabel = getSecondWord(label);
+
+  let toggleAdd = '';
+  let handleToggleAdd = () => {};
+  if (strippedLabel === 'Agency') {
+    const { toggleAddAgency, handleToggleAddAgency } =
+      useContext(AgencyPageContext);
+    toggleAdd = toggleAddAgency;
+    handleToggleAdd = handleToggleAddAgency;
+  } else if (strippedLabel === 'Manager') {
+    const { toggleAddManager, handleToggleAddManager } =
+      useContext(ManagerPageContext);
+    toggleAdd = toggleAddManager;
+    handleToggleAdd = handleToggleAddManager;
+  }
+
   return (
     <div
       className={`flex gap-2 items-center cursor-pointer ${
-        toggleAddAgency ? 'mb-4' : null
+        toggleAdd ? 'mb-4' : null
       }`}
-      onClick={handleToggleAddAgency}
+      onClick={handleToggleAdd}
       {...props}
     >
       <span className="text-xl font-semibold">{label}</span>
-      {toggleAddAgency ? (
+      {toggleAdd ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
