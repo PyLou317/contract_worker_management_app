eed431c83ef3002ebca9e9ffd3bcbe471e731690
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-14 21:13:13 -0800

    cleanup comments

--- frontend/src/pages/Settings/Agencies/AddAgencyForm.jsx
+++ frontend/src/pages/Settings/Agencies/AddAgencyForm.jsx
@@ -102,129 +102,128 @@ export default function AddAgencyForm() {
           toggleAddAgency ? 'max-h-[1000px] p-4 mb-16' : 'max-h-0'
         }`}
       >
-          <form onSubmit={handleSubmit} className="mt-4">
-            <div className="grid grid-cols-1 md:gap-4">
-              <Input
-                label="Agency Name"
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Agency Name"
-                value={formData.name}
-                onChange={handleInputChange}
-                required
-              />
-            </div>
-
-            <h1 className="font-semibold text-md mt-12">Contact:</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-10 mt-8">
-              <Input
-                label="Contact First Name"
-                type="text"
-                id="contact_first_name"
-                name="contact_first_name"
-                placeholder="Contact First Name"
-                onChange={handleInputChange}
-                value={formData.contact_first_name}
-                required
-              />
-              <Input
-                label="Contact Last Name"
-                type="text"
-                id="contact_last_name"
-                name="contact_last_name"
-                placeholder="Contact Last Name"
-                onChange={handleInputChange}
-                value={formData.contact_last_name}
-                required
-              />
-              <Input
-                label="Contact Number"
-                type="text"
-                id="phone_number"
-                name="phone_number"
-                placeholder="Contact Number"
-                onChange={handleInputChange}
-                value={formData.phone_number}
-                required
-              />
-              <Input
-                label="Contact Email"
-                type="text"
-                id="contact_email"
-                name="contact_email"
-                placeholder="Contact Email"
-                onChange={handleInputChange}
-                value={formData.contact_email}
-                required
-              />
-            </div>
-
-            <h1 className="font-semibold text-md mt-12">Address:</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-x-4 mt-10">
-              <Input
-                label="Street"
-                type="text"
-                id="address"
-                name="address"
-                placeholder="Street"
-                onChange={handleInputChange}
-                value={formData.address}
-                required
-              />
-              <Input
-                label="City"
-                type="text"
-                id="city"
-                name="city"
-                placeholder="City"
-                onChange={handleInputChange}
-                value={formData.city}
-                required
-              />
-              <Input
-                label="Province"
-                type="text"
-                id="province"
-                name="province"
-                placeholder="Province"
-                onChange={handleInputChange}
-                value={formData.province}
-                required
-              />
-              <Input
-                label="Postal Code"
-                type="text"
-                id="postal_code"
-                name="postal_code"
-                placeholder="Postal Code"
-                onChange={handleInputChange}
-                value={formData.postal_code}
-                required
-              />
-            </div>
-
-            <h1 className="font-semibold text-md mt-12">Website:</h1>
-            <div className="grid grid-cols-1 md:gap-y-10 md:gap-x-4 mt-10">
-              <Input
-                label="Website"
-                type="text"
-                id="website"
-                name="website"
-                placeholder="Website"
-                onChange={handleInputChange}
-                value={formData.website}
-                required
-              />
-            </div>
-            <SubmitButton
-              type="submit"
-              label="Add Agency"
-              disabled={addAgencyMutation.isPending}
-              className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
+        <form onSubmit={handleSubmit} className="mt-4">
+          <div className="grid grid-cols-1 md:gap-4">
+            <Input
+              label="Agency Name"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Agency Name"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
             />
-          </form>
-        )}
+          </div>
+
+          <h1 className="font-semibold text-md mt-12">Contact:</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-10 mt-8">
+            <Input
+              label="Contact First Name"
+              type="text"
+              id="contact_first_name"
+              name="contact_first_name"
+              placeholder="Contact First Name"
+              onChange={handleInputChange}
+              value={formData.contact_first_name}
+              required
+            />
+            <Input
+              label="Contact Last Name"
+              type="text"
+              id="contact_last_name"
+              name="contact_last_name"
+              placeholder="Contact Last Name"
+              onChange={handleInputChange}
+              value={formData.contact_last_name}
+              required
+            />
+            <Input
+              label="Contact Number"
+              type="text"
+              id="phone_number"
+              name="phone_number"
+              placeholder="Contact Number"
+              onChange={handleInputChange}
+              value={formData.phone_number}
+              required
+            />
+            <Input
+              label="Contact Email"
+              type="text"
+              id="contact_email"
+              name="contact_email"
+              placeholder="Contact Email"
+              onChange={handleInputChange}
+              value={formData.contact_email}
+              required
+            />
+          </div>
+
+          <h1 className="font-semibold text-md mt-12">Address:</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-y-10 md:gap-x-4 mt-10">
+            <Input
+              label="Street"
+              type="text"
+              id="address"
+              name="address"
+              placeholder="Street"
+              onChange={handleInputChange}
+              value={formData.address}
+              required
+            />
+            <Input
+              label="City"
+              type="text"
+              id="city"
+              name="city"
+              placeholder="City"
+              onChange={handleInputChange}
+              value={formData.city}
+              required
+            />
+            <Input
+              label="Province"
+              type="text"
+              id="province"
+              name="province"
+              placeholder="Province"
+              onChange={handleInputChange}
+              value={formData.province}
+              required
+            />
+            <Input
+              label="Postal Code"
+              type="text"
+              id="postal_code"
+              name="postal_code"
+              placeholder="Postal Code"
+              onChange={handleInputChange}
+              value={formData.postal_code}
+              required
+            />
+          </div>
+
+          <h1 className="font-semibold text-md mt-12">Website:</h1>
+          <div className="grid grid-cols-1 md:gap-y-10 md:gap-x-4 mt-10">
+            <Input
+              label="Website"
+              type="text"
+              id="website"
+              name="website"
+              placeholder="Website"
+              onChange={handleInputChange}
+              value={formData.website}
+              required
+            />
+          </div>
+          <SubmitButton
+            type="submit"
+            label="Add Agency"
+            disabled={addAgencyMutation.isPending}
+            className="px-4 py-2 mt-8 w-full bg-yellow-400 font-medium rounded-lg hover:bg-yellow-300 transition-colors"
+          />
+        </form>
       </div>
     </>
   );
