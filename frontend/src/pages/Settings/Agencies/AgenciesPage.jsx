5a27d4831804118f3e1c8ce9c2a99d2c675b8c87
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-14 21:19:55 -0800

    add context value and state

--- frontend/src/pages/Settings/Agencies/AgenciesPage.jsx
+++ frontend/src/pages/Settings/Agencies/AgenciesPage.jsx
@@ -4,6 +4,31 @@ import AddAgencyForm from '@/pages/Settings/Agencies/AddAgencyForm';
 import AgenciesList from '@/pages/settings/Agencies/AgenciesList';
 
 export default function AgenciesPage() {
+  const [toggleAddAgency, setToggleAddAgency] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['agencies'],
+    queryFn: () => apiFetch('/agencies'),
+    keepPreviousData: true,
+  });
+  const agencies = data?.results || [];
+
+  const handleToggleAddAgency = () => {
+    setToggleAddAgency(!toggleAddAgency);
+  };
+
+  const ctxValue = {
+    toggleAddAgency: toggleAddAgency,
+    setToggleAddAgency: setToggleAddAgency,
+    handleToggleAddAgency: handleToggleAddAgency,
+    agencies: agencies,
+    isPending: isPending,
+    isFetching: isFetching,
+    error: error,
+    setShowSuccess: setShowSuccess,
+  };
+
   return (
     <PageContainer>
       <SectionHeader title="Agencies Settings" />
