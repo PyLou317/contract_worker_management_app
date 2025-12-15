323c1e50ffd19f2a5e7fa376cafde04e4185ee90
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-14 21:17:30 -0800

    remove action menu logic

--- frontend/src/pages/Settings/Agencies/AgenciesList.jsx
+++ frontend/src/pages/Settings/Agencies/AgenciesList.jsx
@@ -1,54 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { apiFetch } from '@/utilities/apiClient';
+import { AgencyPageContext } from './agency-page-context';
 
 import AgencyCard from '@/pages/Settings/Agencies/AgencyCard';
-import ActionMenu from '@/components/ActionMenu';
 import LoadingSpinner from '@/components/Loader';
 
 export default function AgenciesList() {
-  const [showActionMenu, setShowActionMenu] = useState(false);
-  const [editingSkillId, setEditingSkillId] = useState(null);
-  const [menuPosition, setMenuPosition] = useState({ top: 0, left: 0 });
+  const { agencies, isPending, isFetching, error } =
+    useContext(AgencyPageContext);
 
-  const { isPending, error, data, isFetching } = useQuery({
-    queryKey: ['agencies'],
-    queryFn: () => apiFetch('/agencies'),
-    keepPreviousData: true,
-  });
+  const [isLoading, setIsLoading] = useState(true);
 
-  const agencies = data?.results || [];
+  useEffect(() => {
+    if (!isPending && !isFetching) {
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
 
-  const handleCloseActionMenu = () => {
-    setShowActionMenu(false);
-    setEditingSkillId(null);
-  };
-
-  const handleOpenActionMenu = (agencyId, event) => {
-    setEditingSkillId(agencyId);
-    setShowActionMenu(true);
-
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    setMenuPosition({
-      top: mouseY,
-      left: mouseX,
-    });
-  };
-
-  if (isPending || isFetching) {
-    return (
-      <div className="mt-8 text-center text-gray-500">
-        Loading agencies...
-        <span>
-          <div className="flex justify-center items-center h-[400px]">
-            <LoadingSpinner size="10" />
-          </div>
-        </span>
-      </div>
-    );
-  }
+      return () => clearTimeout(timer);
+    } else {
+      setIsLoading(true);
+    }
+  }, [isPending, isFetching]);
 
   if (error) {
     return (
@@ -59,43 +32,26 @@ export default function AgenciesList() {
   }
 
   return (
-    <div className="mt-8 lg:w-full">
-      <div className="mb-8">
-        <div className="mb-4">
-          <h1 className="text-2xl font-bold text-gray-800">Active Agencies</h1>
-          <small>Active agencies count: {agencies.length}</small>
+    <div className="mt-4 mb-8 lg:w-full">
+      {isLoading ? (
+        <div className="flex w-full justify-center">
+          <LoadingSpinner size="10" />
         </div>
-
-        {agencies.length > 0 ? (
-          <ul>
-            {agencies.map((agency) => (
-              <AgencyCard
-                key={agency.id}
-                agency={agency}
-                handleEditAgencyClick={(event) =>
-                  handleOpenActionMenu(agency.id, event)
-                }
-              />
-            ))}
-          </ul>
-        ) : (
-          <div className="mt-8 w-full">
-            <div className="mt-4 w-fit mx-auto overflow-x-auto border border-gray-200 bg-gray-100/50 rounded-lg p-4">
-              <p className="text-gray-500">
-                No active agencies found. Please add new agency above.
-              </p>
-            </div>
+      ) : agencies.length > 0 ? (
+        <ul>
+          {agencies.map((agency) => (
+            <AgencyCard key={agency.id} agency={agency} />
+          ))}
+        </ul>
+      ) : (
+        <div className="mt-8 w-full">
+          <div className="mt-4 w-fit mx-auto overflow-x-auto border border-gray-200 bg-gray-100/50 rounded-lg p-4">
+            <p className="text-gray-500">
+              No active agencies found. Please add new agency above.
+            </p>
           </div>
-        )}
-      </div>
-      {showActionMenu && (
-        <ActionMenu
-          skillId={editingSkillId}
-          position={menuPosition}
-          onClose={handleCloseActionMenu}
-        />
+        </div>
       )}
-      {/* <EditSkillModal skillId={editingSkillId} /> */}
     </div>
   );
 }
