8badd1ef4ebfe3bd315ae572d680015bfc4158d2
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-14 21:35:15 -0800

    render outlet based on workerID

--- frontend/src/pages/Workers/WorkerList/WorkersListPage.jsx
+++ frontend/src/pages/Workers/WorkerList/WorkersListPage.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react';
 
 import WorkerListTable from './WorkerListTable';
 import Header from './WorkerTableHeader';
-import SectionHeader from '../../../components/SectionHeader';
+import SectionHeader from '@/components/SectionHeader';
+import PageContainer from '@/components/PageContainer';
+
+import { Outlet, useParams } from 'react-router';
 
 export default function Workers() {
   const searchParams = new URLSearchParams(window.location.search);
@@ -10,6 +13,7 @@ export default function Workers() {
   const [searchTerm, setSearchTerm] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [page, setPage] = useState(initialPage);
+  const { workerId } = useParams();
 
   const handleSearch = (term) => {
     setSearchTerm(term);
@@ -21,20 +25,26 @@ export default function Workers() {
   };
 
   return (
-    <div className="container mx-auto p-8 bg-white shadow-xl rounded-2xl">
-      <SectionHeader title="Employee Roster" />
-      <Header
-        handleSearch={handleSearch}
-        handleOpenAddWorkerModal={handleOpenAddWorkerModal}
-        searchTerm={searchTerm}
-      />
-      <WorkerListTable
-        searchTerm={searchTerm}
-        page={page}
-        setPage={setPage}
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-      />
-    </div>
+    <PageContainer>
+      {workerId ? (
+        <Outlet />
+      ) : (
+        <>
+          <SectionHeader title="Employee Roster" />
+          <Header
+            handleSearch={handleSearch}
+            handleOpenAddWorkerModal={handleOpenAddWorkerModal}
+            searchTerm={searchTerm}
+          />
+          <WorkerListTable
+            searchTerm={searchTerm}
+            page={page}
+            setPage={setPage}
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setIsModalOpen}
+          />
+        </>
+      )}
+    </PageContainer>
   );
 }
