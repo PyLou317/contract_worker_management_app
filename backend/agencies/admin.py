bdbeb63e9c09174c6fd883df85f403139883a078
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-12-16 20:25:52 -0800

    chore: add id to admin fields

--- backend/agencies/admin.py
+++ backend/agencies/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from agencies.models import StaffingAgency
 
-admin.site.register(StaffingAgency)
 
+@admin.register(StaffingAgency)
+class StaffingAgencyAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "organization")
+    list_display_links = ("name",)
+    search_fields = ("name", "organization__name")
+    list_filter = ("organization",)
+    ordering = ("name",)
+    readonly_fields = ("id",)
